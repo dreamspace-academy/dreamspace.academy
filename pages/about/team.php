5df3d39c6673b6c59ae15343bff2b293ab314496
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-05 11:36:45 +0200

    Update team detail of aravinth

--- pages/about/team.php
+++ pages/about/team.php
@@ -1,6 +1,4 @@
-
-
-			<!-- Section -->
+<!-- Section -->
 			<div class="template-content-section template-padding-top-reset template-padding-bottom-5 template-main">
 
 				<!-- Header and subheader -->
@@ -12,17 +10,17 @@
 
 				<!-- Layout 50x50 -->
 				<div class="template-layout-50x50 template-clear-fix">
-					
+
 					<!-- Left column -->
 					<div class="template-layout-column-left">
 						<!--<h4>Our staff consists of three teachers and one assistant with experience in early childhood care.</h4>
 					</div>
-					
+
 					<!-- Right column -->
 					<div class="template-layout-column-right">
 						<!--<p>Praesent arcu gravida vehicula est node maecenas loareet morbi a dosis luctus. Urna eget lacinia eleifend praesent luctus a arcu quis facilisis venenatis. Aenean interdum, nibh vitae sodales, magna ante feugiat elit maecenas.</p> -->
 					</div>
-					
+
 				</div>
 
 				<!-- Team -->
@@ -41,7 +39,7 @@
 											<span>Co-founder & Director, DreamSpace</span>
 										</div>
 										<p><b>Kishoth Navaretnarajah</b> Co-founder & Director, DreamSpace</p>
-									</div>					
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
@@ -50,9 +48,9 @@
 										<ul>
 											<li><a href="https://www.linkedin.com/in/kishoth-navaretnarajah-342b4311a/" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>					
+									</div>
 								</li>
-							</ul>			
+							</ul>
 						</li>
 						<li class="template-layout-column-right">
 							<ul class="template-layout-50x50 template-clear-fix">
@@ -64,22 +62,22 @@
 										</a>
 										<div>
 											<h6>Aravinth Panch</h6>
-											<span>Co-founder & Director of Innovation, DreamSpace</span>
+											<span>Co-founder & Director of Innovation</span>
 										</div>
-										<p><b>Aravinth Panch</b> Co-founder & Director of Innovation, DreamSpace</p>
-									</div>					
+										<p><b>Aravinth Panch</b> Co-founder & Director of Innovation</p>
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
-									<p class="template-component-team-description">My name is Katie. I grew up and studied in Canada. This is my second year at Fable and love every minute of it, making the children’s learning experience fun.</p>
+									<p class="template-component-team-description">Aravinth is a Community Builder, Embedded System Engineer and New Media Artist, worked on various software, hardware, art & social projects for more than 15 years. He curates communities in the domain of startups, sustainability, education and art in Berlin, Europe & Asia.</p>
 									<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
 										<ul>
-											
+
 											<li><a href="https://www.linkedin.com/in/aravinthpanch/" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>						
+									</div>
 								</li>
-							</ul>				
+							</ul>
 						</li>
 						<li class="template-layout-column-left">
 							<ul class="template-layout-50x50 template-clear-fix">
@@ -88,13 +86,13 @@
 										<a href="../../media/images/about/organisation/maite-growth-strategist-dreamspace.png" data-fancybox-group="team-2">
 											<img src="../../media/images/about/organisation/maite-growth-strategist-dreamspace.png" alt="maite-growth-strategist-dreamspace" />
 											<span><span><span></span></span></span>
-										</a>   
+										</a>
 										<div>
 											<h6>Maite Hernandez Zubeldia</h6>
 											<span>Growth Strategist of DreamSpace and Design Thinking</span>
 										</div>
 										<p><b>Maite Hernandez Zubeldia</b> Growth Strategist of DreamSpace and Design Thinking</p>
-									</div>					
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
@@ -103,9 +101,9 @@
 										<ul>
 											<li><a href="https://www.linkedin.com/in/maitezubeldia/" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>					
+									</div>
 								</li>
-							</ul>			
+							</ul>
 						</li>
 						<li class="template-layout-column-right">
 							<ul class="template-layout-50x50 template-clear-fix">
@@ -120,19 +118,19 @@
 											<span>Head of Administration, DreamSpace</span>
 										</div>
 										<p><b>Thanoochkumar Thayaleswararajah</b> Head of Administration, DreamSpace</p>
-									</div>					
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
 									<p class="template-component-team-description">My name is Katie. I grew up and studied in Canada. This is my second year at Fable and love every minute of it, making the children’s learning experience fun.</p>
 									<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
 										<ul>
-											
+
 											<li><a href="https://www.linkedin.com/in/thanooch-kumar-689730184/" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>						
+									</div>
 								</li>
-							</ul>				
+							</ul>
 						</li>
 						<li class="template-layout-column-left">
 							<ul class="template-layout-50x50 template-clear-fix">
@@ -147,7 +145,7 @@
 											<span>Human Relations, DreamSpace</span>
 										</div>
 										<p><b>Prasath  Thevapalan</b> Human Relations, DreamSpace</p>
-									</div>					
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
@@ -156,9 +154,9 @@
 										<ul>
 											<li><a href="https://www.linkedin.com/in/" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>						
+									</div>
 								</li>
-							</ul>				
+							</ul>
 						</li>
 						<li class="template-layout-column-right">
 							<ul class="template-layout-50x50 template-clear-fix">
@@ -173,7 +171,7 @@
 											<span>Teacher</span>
 										</div>
 										<p><b>Thanushkar Srikaran</b> Teacher</p>
-									</div>					
+									</div>
 								</li>
 								<li class="template-layout-column-right">
 									<div class="template-component-team-quote"></div>
@@ -182,12 +180,11 @@
 										<ul>
 											<li><a href="https://www.linkedin.com/in/thanushkar-srikaran-203285178" class="template-component-social-icon-linkedin"></a></li>
 										</ul>
-									</div>						
+									</div>
 								</li>
-							</ul>				
+							</ul>
 						</li>
 					</ul>
 				</div>
 
 			</div>
-			
