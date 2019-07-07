3a729c7f402f147e84eb7042baf83dca8cca116a
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-07 15:22:36 +0200

    Add community pages, Remove unwanted files

---
+++ pages/communities/chess.php
@@ -0,0 +1,60 @@
+<!-- Section -->
+<div class="template-content-section template-padding-bottom-5 template-background-color-2">
+
+	<!-- Main -->
+	<div class="template-main">
+
+		<!-- Layout 50x50 -->
+		<div class="template-layout-50x50 template-clear-fix">
+
+			<!-- Left column -->
+			<div class="template-layout-column-left template-margin-bottom-reset">
+
+				<!-- Gallery -->
+				<div class="template-component-gallery">
+					<ul class="template-layout-100 template-clear-fix">
+						<li class="template-layout-column-left">
+							<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+								<a href="../../media/images/communities/chess.jpg" data-fancybox-group="gallery-1">
+									<img src="../../media/images/communities/chess.jpg" alt="" />
+									<span><span><span></span></span></span>
+								</a>
+								<div>
+									<h6>Drawing and Painting Lessons</h6>
+									<span>Tenderhearts Class</span>
+								</div>
+								<p><b>Drawing and Painting Lessons</b> Tenderhearts Class</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+			<!-- Right column -->
+			<div class="template-layout-column-right template-margin-bottom-reset">
+				<div class="template-align-center">
+					<h2>Chess</h2>
+					<div class="template-component-divider template-component-divider-style-2"></div>
+					<div class="template-component-italic template-margin-top-3">
+						Our chess community is actively training the young minds to grasp the intricacies of the game
+					</div>
+					<p class="template-margin-top-3">
+						It is widely accepted by most historians that the game of chess originated in India in the fifth or sixth century AD.
+					</p>
+					<p class="template-margin-top-3">
+						The earliest known form of chess is two-handed chaturanga, Sanskrit for “the four branches of the army.” Like real Indian armies at that time, the pieces were called elephants, chariots, horses, and foot soldiers. Unlike modern chess,
+						chaturanga was mainly a game of chance; results depended on how well you rolled the dice.
+					</p>
+					<p class="template-margin-top-3">
+						From India, chaturanga spread quickly to Persia, where it was called chatrang. When Arabs invaded Persia in the seventh century, they called it shatranj and popularized it throughout the Arab world. Chess made its way to Europe in the tenth
+						century as a result of the Arab expansion.
+					</p>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
--- pages/communities/index.php
+++ pages/communities/index.php
@@ -1,9 +1,37 @@
+<!-- header-menu -->
 <?php include('../master-page/header.php') ?>
+
 <!-- Content -->
 <div class="template-content">
-	<!-- organisation-section -->
-	<section id="organisation">
-		<?php include('organisation.php') ?>
+
+	<!-- Header and subheader -->
+	<div class="template-component-header-subheader">
+		<h2>Communities</h2>
+		<h6></h6>
+		<div></div>
+	</div>
+
+	<!-- community-innovation section -->
+	<section id="community-innovation">
+		<?php include('community-innovation.php') ?>
 	</section>
+
+	<!-- terrace section -->
+	<section id="terrace">
+		<?php include('terrace.php') ?>
+	</section>
+
+	<!-- urban-farming section -->
+	<section id="urban-farming">
+		<?php include('urban-farming.php') ?>
+	</section>
+
+	<!-- chess section -->
+	<section id="chess">
+		<?php include('chess.php') ?>
+	</section>
+
 </div>
+
+<!-- footer-menu -->
 <?php include('../master-page/footer.php') ?>
