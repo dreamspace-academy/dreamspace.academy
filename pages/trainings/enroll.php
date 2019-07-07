29659c6cdd10eef49b9cb93dbd1bc113407cbbec
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-07 15:27:34 +0200

    Update trainings page, Remove unwanted files

---
+++ pages/trainings/enroll.php
@@ -0,0 +1,33 @@
+<!-- Section -->
+<div class="template-content-section template-padding-top-reset template-padding-bottom-reset template-background-color-1">
+  <div class="template-main">
+
+    <!-- Call to action -->
+    <div class="template-component-call-to-action template-component-call-to-action-style-1">
+
+      <!-- Content -->
+      <div class="template-component-call-to-action-content">
+
+        <!-- Left column -->
+        <div class="template-component-call-to-action-content-left">
+
+          <!-- Header -->
+          <h3 style="color:white;">How to Enroll Your Child to a Class?</h3>
+
+        </div>
+
+        <!-- Right column -->
+        <div class="template-component-call-to-action-content-right">
+
+          <!-- Button -->
+          <a href="../../contact-us.php" class="template-component-button template-component-button-style-1">Click Here<i></i></a>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
--- pages/trainings/index.php
+++ pages/trainings/index.php
@@ -1,4 +1,6 @@
-<?php include('header.php'); ?>
+<!-- header-menu -->
+<?php include('../master-page/header.php') ?>
+
 <!-- Content -->
 <div class="template-content">
 
@@ -26,43 +28,9 @@
 		<?php include('studies-of-languages.php'); ?>
 	</section>
 
-
-
-
-	<!-- Section -->
-	<div class="template-content-section template-padding-top-reset template-padding-bottom-reset template-background-color-1">
-		<div class="template-main">
-
-			<!-- Call to action -->
-			<div class="template-component-call-to-action template-component-call-to-action-style-1">
-
-				<!-- Content -->
-				<div class="template-component-call-to-action-content">
-
-					<!-- Left column -->
-					<div class="template-component-call-to-action-content-left">
-
-						<!-- Header -->
-						<h3 style="color:white;">How to Enroll Your Child to a Class?</h3>
-
-					</div>
-
-					<!-- Right column -->
-					<div class="template-component-call-to-action-content-right">
-
-						<!-- Button -->
-						<a href="../../contact-us.php" class="template-component-button template-component-button-style-1">Click Here<i></i></a>
-
-					</div>
-
-				</div>
-
-			</div>
-
-		</div>
-
-	</div>
+	<?php include('enroll.php'); ?>
 
 </div>
 
-<?php include('footer.php'); ?>
+<!-- footer-menu -->
+<?php include('../master-page/footer.php') ?>
