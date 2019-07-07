64904c2fc7a43c2e0b75ffbe1f1e65348c94333e
Viththiyakaran <battikaran6@gmail.com>
2019-07-07 20:36:10 +0530

    Update partner image at about

--- pages/about/partners.php
+++ pages/about/partners.php
@@ -16,36 +16,66 @@
 
 			<ul class="template-layout-100">
 
-				<li class="template-layout-column-left">
-
-					<img src="../../media/images/partners-logo/sylc.jpg" style="width:50%;margin: auto;">
-					<div></div>
-					<span>SYLC</span>
-				</li>
+				
 
 				<li class="template-layout-column-left">
 					<img src="../../media/images/partners-logo/bookbridge.jpg" style="width:20%;margin: auto;">
 					<div></div>
 					<span>BookBridge</span>
 				</li>
-
 				<li class="template-layout-column-left">
-					<img src="../../media/images/partners-logo/cpbr.jpg" style="width:20%;margin: auto;">
+					<img src="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" style="width:50%; margin: auto;">
 					<div></div>
-					<span>Cpbr</span>
+					<span>UN Volunteers Sri Lanka</span>
 				</li>
+				<li class="template-layout-column-left">
 
+					<img src="../../media/images/partners-logo/sylc.jpg" style="width:50%;margin: auto;">
+					<div></div>
+					<span>SYLC</span>
+				</li>
 				<li class="template-layout-column-left">
 					<img src="../../media/images/partners-logo/motionlab.jpg" style="width:20%;margin: auto;">
 					<div></div>
 					<span>MotionLab</span>
 				</li>
-
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/cpbr.jpg" style="width:20%;margin: auto;">
+					<div></div>
+					<span>Cpbr</span>
+				</li>
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/troublemaker.jpg" style="width:20%;margin: auto;">
+					<div></div>
+					<span>TroubleMaker</span>
+				</li>
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/steamhead.jpg" style="width:20%;margin: auto;">
+					<div></div>
+					<span>STEAM Head</span>
+				</li>
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/greenbuzz.png" style="width:20%;margin: auto;">
+					<div></div>
+					<span>GreenBuzz</span>
+				</li>
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/gosh.png" style="width:20%;margin: auto;">
+					<div></div>
+					<span>Gathering for Open Science Hardware</span>
+				</li>
+				<li class="template-layout-column-left">
+					<img src="../../media/images/partners-logo/gig.jpg" style="width:20%;margin: auto;">
+					<div></div>
+					<span>Global Innovation Gathering</span>
+				</li>
+				
+				
 
 				<li class="template-layout-column-left">
-					<img src="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" style="width:50%; margin: auto;">
+					<img src="../../media/images/partners-logo/shdb.jpg" style="width:20%;margin: auto;">
 					<div></div>
-					<span>UN Volunteers Sri Lanka</span>
+					<span>Science Hack Day Berlin</span>
 				</li>
 				<li class="template-layout-column-left">
 					<img src="../../media/images/partners-logo/scout.png" style="width:20%;margin: auto;">
