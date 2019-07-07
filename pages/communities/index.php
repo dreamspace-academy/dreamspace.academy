c9e9923b83e6895c43331ddf783a3a8b63d0c537
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-07 14:57:14 +0200

    Remove individual pages for communities

---
+++ pages/communities/index.php
@@ -0,0 +1,9 @@
+<?php include('../master-page/header.php') ?>
+<!-- Content -->
+<div class="template-content">
+	<!-- organisation-section -->
+	<section id="organisation">
+		<?php include('organisation.php') ?>
+	</section>
+</div>
+<?php include('../master-page/footer.php') ?>
