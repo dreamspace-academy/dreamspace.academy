34d71233018881cad00e27427e86f3cef23ccf2d
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-27 01:25:22 +0700

    Fix layout, image size, image subtile, image link

--- pages/about/partners.php
+++ pages/about/partners.php
@@ -1,181 +1,200 @@
 <!-- Section -->
 <div class="template-content-section template-padding-bottom-reset template-main template-clear-fix">
 
-
 	<!-- Header and subheader -->
 	<div class="template-component-header-subheader">
 		<h2>Our Partners</h2>
 		<h6></h6>
 		<div></div>
 	</div>
-	
+
 	<!-- Section -->
 	<div data-id="section-1">
-		
-		
+
 		<!-- Gallery -->
 		<div class="template-component-gallery">
 			<ul class="template-layout-33x33x33 template-clear-fix">
 				<li class="template-layout-column-left">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/bookbridge.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/bookbridge.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://www.bookbridge.org/">BookBridge</a></h6>
-							
-						</div>
-						<p><a href="https://www.bookbridge.org/">BookBridge</a></p>
-					</div>
-				</li>
-				<li class="template-layout-column-center">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="http://unvlk.org/">UN Volunteers</a></h6>
-							
-						</div>
-						<p><a href="http://unvlk.org/">UN Volunteers</a></p>
-					</div>			
-				</li>
-				<li class="template-layout-column-right">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/sylc.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/sylc.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://sylcsl.org/">Sagacious Youth Lead Consortium</a></h6>
-							
-						</div>
-						<p><a href="https://sylcsl.org/">Sagacious Youth Lead Consortium</a></p>
-					</div>			
-				</li>
-				
-				<li class="template-layout-column-left">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/motionlab.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/motionlab.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://motionlab.berlin/">Motion Lab</a></h6>
-							
-						</div>
-						<p><a href="https://motionlab.berlin/">Motion Lab</a></p>
-					</div>
-				</li>
-				<li class="template-layout-column-center">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/cpbr.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/cpbr.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-						<h6><a href="https://www.peaceinsight.org/conflicts/sri-lanka/peacebuilding-organisations/cpbr/">CPBR</a></h6>
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
 
+							<a href="../../media/images/partners-logo/bookbridge.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/bookbridge.jpg" alt="">
+							</a>
+							<div>
+								<h6><a target=_blank href="https://www.bookbridge.org/">BookBridge, Switzerland | Global</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://www.bookbridge.org/">BookBridge, Switzerland | Global</a></p>
 						</div>
-						<p><a href="https://www.peaceinsight.org/conflicts/sri-lanka/peacebuilding-organisations/cpbr/">CPBR</a></p>
-					</div>			
-				</li>
-				<li class="template-layout-column-right">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/troublemaker.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/troublemaker.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://troublemaker.site/wp/">Troublemaker</a></h6>
-							
-						</div>
-						<p><a href="https://troublemaker.site/wp/">Troublemaker</a></p>
-					</div>			
-				</li>
-				<li class="template-layout-column-left">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/steamhead.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/steamhead.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://steamhead.space/">Steamhead</a></h6>
-							
-						</div>
-						<p><a href="https://steamhead.space/">Steamhead</a></p>
-					</div>
+					</center>
 				</li>
 				<li class="template-layout-column-center">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/greenbuzz.png" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/greenbuzz.png" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://greenbuzzberlin.de/">Greenbuzz</a></h6>
-							
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/un-volunteers-sri-lanka.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="http://unvlk.org/">UN Volunteers, Sri Lanka</a></h6>
+
+							</div>
+							<p><a target=_blank href="http://unvlk.org/">UN Volunteers, Sri Lanka</a></p>
 						</div>
-						<p><a href="https://greenbuzzberlin.de/">Greenbuzz</a></p>
-					</div>			
+					</center>
 				</li>
 				<li class="template-layout-column-right">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/gosh.png" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/gosh.png" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="http://openhardware.science/">GOSH</a></h6>
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/sylc.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/sylc.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://sylcsl.org/">Sagacious Youth Lead Consortium, Sri Lanka</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://sylcsl.org/">Sagacious Youth Lead Consortium, Sri Lanka</a></p>
 						</div>
-						<p><a href="http://openhardware.science/">GOSH</a></p>
-					</div>			
+					</center>
 				</li>
 				<li class="template-layout-column-left">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/gig.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/gig.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://www.globalinnovationgathering.org/">GIG</a></h6>
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/motionlab.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/motionlab.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://motionlab.berlin/">MotionLab, Germany</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://motionlab.berlin/">MotionLab, Germany</a></p>
 						</div>
-						<p><a href="https://www.globalinnovationgathering.org/">GIG</a></p>
-					</div>
+					</center>
 				</li>
 				<li class="template-layout-column-center">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/shdb.jpg" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/shdb.jpg" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="http://berlin.sciencehackday.org/">Science Hack Day </a></h6>
-							
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/cpbr.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/cpbr.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://www.peaceinsight.org/conflicts/sri-lanka/peacebuilding-organisations/cpbr/">CPBR, Sri Lanka</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://www.peaceinsight.org/conflicts/sri-lanka/peacebuilding-organisations/cpbr/">CPBR, Sri Lanka</a></p>
 						</div>
-						<p><a href="http://berlin.sciencehackday.org/">Science Hack Day </a></p>
-					</div>			
+					</center>
 				</li>
 				<li class="template-layout-column-right">
-					<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<a href="../../media/images/partners-logo/scout.png" data-fancybox-group="gallery-2">
-							<img src="../../media/images/partners-logo/scout.png" alt="">
-							<span><span><span></span></span></span>
-						</a>
-						<div>
-							<h6><a href="https://en.wikipedia.org/wiki/Sri_Lanka_Scout_Association">Scout</a></h6>
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/troublemaker.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/troublemaker.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://troublemaker.site/wp/">Troublemaker, China</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://troublemaker.site/wp/">Troublemaker, China</a></p>
 						</div>
-						<p><a href="https://en.wikipedia.org/wiki/Sri_Lanka_Scout_Association">Scout</a></p>
-					</div>			
+					</center>
+				</li>
+				<li class="template-layout-column-left">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/steamhead.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/steamhead.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://steamhead.space/">SteamHead, China | USA</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://steamhead.space/">SteamHead, China | USA</a></p>
+						</div>
+					</center>
+				</li>
+				<li class="template-layout-column-center">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/greenbuzz.png" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/greenbuzz.png" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://greenbuzzberlin.de/">GreenBuzz, Germany</a></h6>
+
+							</div>
+							<p><a target=_blank href="https://greenbuzzberlin.de/">GreenBuzz, Germany</a></p>
+						</div>
+					</center>
+				</li>
+				<li class="template-layout-column-right">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/gosh.png" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/gosh.png" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="http://openhardware.science/">Gathering for Open Science Hardware, Global</a></h6>
+							</div>
+							<p><a target=_blank href="http://openhardware.science/">Gathering for Open Science Hardware, Global</a></p>
+						</div>
+					</center>
+				</li>
+				<li class="template-layout-column-left">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/gig.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/gig.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://www.globalinnovationgathering.org/">Global Innovation Gathering, Germany | Global</a></h6>
+							</div>
+							<p><a target=_blank href="https://www.globalinnovationgathering.org/">Global Innovation Gathering, Germany | Global</a></p>
+						</div>
+					</center>
+				</li>
+				<li class="template-layout-column-center">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/shdb.jpg" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/shdb.jpg" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="http://berlin.sciencehackday.org/">Science Hack Day, Germany | Global</a></h6>
+
+							</div>
+							<p><a target=_blank href="http://berlin.sciencehackday.org/">Science Hack Day, Germany | Global</a></p>
+						</div>
+					</center>
+				</li>
+				<li class="template-layout-column-right">
+					<center>
+						<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+							<a href="../../media/images/partners-logo/scout.png" data-fancybox-group="gallery-2">
+								<img style="height:50%; width:50%" src="../../media/images/partners-logo/scout.png" alt="">
+
+							</a>
+							<div>
+								<h6><a target=_blank href="https://en.wikipedia.org/wiki/Sri_Lanka_Scout_Association">Scout Association, Sri Lanka</a></h6>
+							</div>
+							<p><a target=_blank href="https://en.wikipedia.org/wiki/Sri_Lanka_Scout_Association">Scout Association, Sri Lanka</a></p>
+						</div>
+					</center>
 				</li>
 			</ul>
 		</div>
 
-			</div>
-
+	</div>
 
 
 </div>
-
