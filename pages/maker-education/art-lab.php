408c4378b0655319c6f668c0deff0228715b2a4f
Aravinth Panch <me@aravinth.info>
2020-02-13 19:48:59 +0530

    Add art lab photos

--- pages/maker-education/art-lab.php
+++ pages/maker-education/art-lab.php
@@ -12,9 +12,11 @@
 			<!-- Nivo slider -->
 			<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
 				<div>
-					<img src="../../media/images/maker-education/studies-of-languages-photo-1.jpg" data-thumb="../../media/images/maker-education/studies-of-languages-photo-1.jpg" alt="studies-of-languages-dreamspace-batticaloa" />
-					<img src="../../media/images/maker-education/studies-of-languages-photo-2.jpg" data-thumb="../../media/images/maker-education/studies-of-languages-photo-2.jpg" alt="studies-of-languages-dreamspace-batticaloa" />
-					<img src="../../media/images/maker-education/studies-of-languages-photo-3.jpg" data-thumb="../../media/images/maker-education/studies-of-languages-photo-3.jpg" alt="studies-of-languages-dreamspace-batticaloa" />
+					<img src="../../media/images/maker-education/art/dreamspace-art-lab-2.jpg" data-thumb="../../media/images/maker-education/art/dreamspace-art-lab-2.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../../media/images/maker-education/art/dreamspace-art-lab-3.jpg" data-thumb="../../media/images/maker-education/art/dreamspace-art-lab-3.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../../media/images/maker-education/art/dreamspace-art-lab-4.jpg" data-thumb="../../media/images/maker-education/art/dreamspace-art-lab-4.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../../media/images/maker-education/art/dreamspace-art-lab-5.jpg" data-thumb="../../media/images/maker-education/art/dreamspace-art-lab-5.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../../media/images/maker-education/art/dreamspace-art-lab-6.jpg" data-thumb="../../media/images/maker-education/art/dreamspace-art-lab-6.jpg" alt="maker-education-dreamspace-academy" />
 				</div>
 			</div>
 		</div>
