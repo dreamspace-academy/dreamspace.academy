7fcaa5d0f4f9f08b06fdd3e2cf9416f2fb0e5d85
Aravinth Panch <me@aravinth.info>
2020-02-22 18:28:21 +0530

    Add public event calendar

---
+++ pages/master-page/calendar.php
@@ -0,0 +1,20 @@
+<?php include('header.php'); ?>
+
+<div class="template-content">
+  <!-- Main -->
+  <div class="template-content-section template-main">
+    <!-- Header and subheader -->
+    <div class="template-component-header-subheader">
+      <h2>Calendar</h2>
+      <h6></h6>
+      <div></div>
+    </div>
+
+    <iframe src="https://calendar.google.com/calendar/b/4/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FColombo&amp;src=ZHJlYW1zcGFjZS5hY2FkZW15X3ExdTg3cnFjdGZxdG9mMXJ0c2JxNnBta204QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%233f51b5&amp;showTitle=0&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;mode=WEEK"
+      style="border-width:0" width="1080" height="600" frameborder="0" scrolling="no"></iframe>
+    </center>
+
+  </div>
+</div>
+
+<?php include('footer.php'); ?>
