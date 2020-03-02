353237e653f143f3f2b5d6f6520a70697b41a8a3
Aravinth Panch <me@aravinth.info>
2020-03-02 23:08:06 +0530

    Update Underwater Glider content

--- pages/community-innovation/underwater-glider.php
+++ pages/community-innovation/underwater-glider.php
@@ -10,44 +10,16 @@
     <!-- Header and subheader -->
     <div class="template-component-header-subheader">
       <h2>Underwater Glider</h2>
-      <h6>An autonomous underwater drone to protect our blue environment from pollutions</h6>
+      <h6>An autonomous underwater vehicle to keep an eye on our Blue Ecosystems</h6>
       <div></div>
     </div>
 
-    <!-- SDGs -->
-    <div class="template-component-feature template-component-feature-style-1 template-component-feature-position-left template-component-feature-size-medium">
-      <ul class="template-layout-33x33x33 template-clear-fix">
-        <li class="template-layout-column-left">
-          <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-14.png" alt="un-sdg-dreamspace" /></div>
-          <h5>Life Below Water</br></br></h5>
-          <p><b>UN SDG 14</b></p>
-          <p>Conserve and sustainably use the oceans, seas and marine resources for sustainable development</p>
-        </li>
-
-        <li class="template-layout-column-center">
-          <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-6.png" alt="un-sdg-dreamspace" /></div>
-          <h5>Clean Water and Sanitation</h5>
-          <p><b>UN SDG 6</b></p>
-          <p>Ensure availability and sustainable management of water and sanitation for all</p>
-        </li>
-
-        <li class="template-layout-column-right">
-          <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-13.png" alt="un-sdg-dreamspace" /></div>
-          <h5>Climate Action</br></br></h5>
-          <p><b>UN SDG 13</b></p>
-          <p>Take urgent action to combat climate change and its impacts</p>
-        </li>
-      </ul>
-    </div>
-
-    <div class="template-component-divider"></div>
-
     <!-- Post -->
-    <div class="template-post" style="padding-top: 50px">
+    <div class="template-post">
 
       <!-- Icon -->
       <div class="template-post-section-icon">
-        <div class="template-post-icon template-post-icon-image"></div>
+        <div class="template-post-icon template-post-icon-sticky"></div>
       </div>
 
       <!-- Preambule -->
@@ -85,48 +57,111 @@
       <!-- Content -->
       <div class="template-post-section-content">
         <div class="template-post-content">
-
-          <h4 class="template-margin-top-3">Gravida maecenas node</h4>
-          <p class="template-padding-reset">
-            Maecenas blandit accumsan lacus ut velit fermentum elementum.
-            Morbi ornare, lacus non consequat consectetur, est sem varius elit, eget congue est ligula non nunc.
-            Suspendisse condimentum mattis odio vitae aliquet. Duis ultricies massa. Phasellus in augue, nulla integer pellentesque.
-            In nunc ut maecenas. Aliquam erat volutpat. Vivamus viverra odio sem, eleifend sagittis. Gratio a meteo, morbi auctor.
+          <h4 class="template-margin-top-3">Motivation</h4>
+          <h6 class="template-margin-top-3">Blue Economy</h6>
+          <p class="template-padding-reset" align="justify">
+            Sri Lanka is an island country, located in the Indian Ocean, with a coastline of 1,585 km long, and the country claims an Exclusive Economic Zone (EEZ) extending 200 nautical miles, which is approximately 6.7 times Sri Lanka's land area.
           </p>
-          <h5 class="template-margin-top-3">Novum not est blandit</h5>
-          <p class="template-padding-reset">
-            Risus magna, accumsan a nunc ac, blandit aliquet neque. Cras sodales nisi id sapien laoreet, non porta tellus elementum.
-            Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris quis eleifend felis. Nunc rutrum luctus sapien vel rutrum.
-            Nulla lobortis tempus erat sed tristique.
+          <p class="template-padding-reset" align="justify">
+            The coastline and adjacent waters support highly productive marine ecosystems provide a massive economic value to this small nation, while this unique geographical position is highly contested by global powers.
+          </p>
+          <a target="_blank" href="http://www.ft.lk/article/620407/Oceans-and-Sri-Lanka-s-future--Towards-a-blue-economy">[more]</a>
+          <h6 class="template-margin-top-3">Life Below Water</h6>
+          <p class="template-padding-reset" align="justify">
+            The world’s oceans – their temperature, chemistry, currents and life – drive global systems that make the Earth habitable for humankind. How we manage this vital resource is essential for humanity as a whole, and to counterbalance the
+            effects of climate change.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            DreamSpace Academy adopts the sustainable development goals (UN SDG) to sustainably manage and protect marine and coastal ecosystems from pollution, as well as address the impacts of ocean acidification.
+          </p>
+          <a target="_blank" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals/goal-14-life-below-water.html">[more]</a>
+
+          <h4 class="template-margin-top-3">Problem</h4>
+          <h6 class="template-margin-top-3">Real-time Monitoring</h6>
+          <p class="template-padding-reset" align="justify">
+            To keep our water ecosystem safe, secure, clean and sustainable, we need to measure various parameters in real-time. Sri Lanka, being a country that was devastated by 30 years of war, doesn’t even have the basic infrastructure, such as
+            roads, in many parts of the country.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            There are no modern devices in Sri Lanka to measure and monitor the water ecosystem. Such equipment is very crucial for scientific research and assessment on an ecosystem, which is swiftly changing due to various weather conditions.
+          </p>
+          <a target="_blank" href="https://ec.europa.eu/maritimeaffairs/policy/ocean-governance_en">[more]</a>
+
+          <h6 class="template-margin-top-3">Affordability</h6>
+          <p class="template-padding-reset" align="justify">
+            There are several solutions, such as Autonomous underwater vehicles (AUVs), to gather real-time oceanographic data for an extended duration and distance with very little human effort. However, such commercial solutions could cost around
+            200,000 € and needs very special expertise & cost to maintain and repair such systems.
           </p>
 
-          <h6 class="template-margin-top-3">Pulvinar modest</h6>
-          <p class="template-padding-reset">
-            Turpis magna, lacinia vitae vehicula eu, blandit vel dolor. Sed volutpat lacus nulla odio, vitae blandit nunc vestibulum.
-            Morbi a lobortis est quis metus efficitur commodo. Malesuada auctor tincidunt.
-            Suspendisse potenti eleifend aliquet tortor, non congue quam presto consectetur.
-            Nullam modest aliquam massa.
+          <h4 class="template-margin-top-3">Solution</h4>
+          <h6 class="template-margin-top-3">Open-source Underwater Glider (OSUG)</h6>
+          <p class="template-padding-reset" align="justify">
+            There has been a breakthrough with low cost autonomous underwater vehicles and as this capability has matured a wide range of hobby and commercial applications have developed. There are no affordable extended duration underwater
+            exploration platforms and this project aims to address this need.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            <a target="_blank" href="https://www.linkedin.com/in/alexwxyz/">Alex Williams</a>, a young inventor from the USA, has developed a low cost and versatile underwater glider capable of extended missions of up to weeks at a time; using
+            commodity hardware, 3D printed parts and an open-source autopilot.
+            Furthermore, he won the Grand Prize in the Hackaday Prize Challenge 2017.
+          </p>
+          <a target="_blank" href="https://hackaday.io/project/20458-osug-open-source-underwater-glider">[more]</a>
+
+          <h6 class="template-margin-top-3">Underwater Gliding</h6>
+          <p class="template-padding-reset" align="justify">
+            Underwater gliders use a buoyancy engine to change the mass of the glider, allowing them to ascend and descend through the water. With power only being used to power the engine intermittently, gliders can typically run for weeks or
+            months without recharge, making them ideal for environmental monitoring. As underwater gliders travel slowly through the water, they disturb the surrounding water very little, allowing for accurate and reliable data recording.
+          </p>
+          <a target="_blank" href="https://auvac.org/newsitems/view/3012">[more]</a>
+
+          <h4 class="template-margin-top-3">Maker</h4>
+          <p class="template-padding-reset" align="justify">
+            <a target="_blank" href="http://www.dailynews.lk/2019/09/12/tc/196709/walk-talk">Shanjeevan Amalanathan</a> is a young Maker from Batticaloa, Sri Lanka. To reinforce his innovative thinking, he joined DreamSpace Academy in 2019, and has
+            been developing the necessary Maker skills to develop Electronics, Mechanics and Softwares. After finishing his G.C.E Advanced Level in 2019, he decided to take a break, before he goes to University, to explore the ocean ecosystem in Sri
+            Lanka.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            During his study, he found out that after the Tsunami disaster in 2004, the ocean ecosystem in Sri Lanka was significantly disrupted, and the local community started seeing a large number of eels that had emerged from the backwaters in
+            Batticaloa, which locals presumed to be snakes, as such species were not commonly visible above water.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            He saw the need for a system to monitor the ocean and backwaters around Batticaloa - a city on the eastern coast of Sri Lanka. Therefore he has taken the challenge to build an affordable underwater glider, with the help of the existing
+            open-source project OSUG.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            He strongly believes that he will be further contributing to the OSUG project, by making it a production-ready underwater glider with multiparameter measurements system that will play a vital role in protecting the Blue Ecosystems in Global South.
           </p>
 
-          <div class="template-component-list template-component-list-style-1 template-margin-top-3">
-            <ul>
-              <li>Comprehensive reporting on individual achievement</li>
-              <li>Educational field trips and school presentations</li>
-              <li>Individual attention in a small-class setting</li>
+          <!-- SDGs -->
+          <h4 class="template-margin-top-5 template-margin-bottom-5">This innovation collects SDG Badges for</h4>
+          <div class="template-component-feature template-component-feature-style-1 template-component-feature-position-left template-component-feature-size-medium">
+            <ul class="template-layout-33x33x33 template-clear-fix">
+              <li class="template-layout-column-left">
+                <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-14.png" alt="un-sdg-dreamspace" /></div>
+                <h5>Life Below Water</br></br></h5>
+                <p><b>UN SDG 14</b></p>
+                <p>Conserve and sustainably use the oceans, seas and marine resources for sustainable development</p>
+              </li>
+
+              <li class="template-layout-column-center">
+                <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-6.png" alt="un-sdg-dreamspace" /></div>
+                <h5>Clean Water and Sanitation</h5>
+                <p><b>UN SDG 6</b></p>
+                <p>Ensure availability and sustainable management of water and sanitation for all</p>
+              </li>
+
+              <li class="template-layout-column-right">
+                <div class="template-icon-feature"><img src="../../media//images/sdg-icons/un-sdg-13.png" alt="un-sdg-dreamspace" /></div>
+                <h5>Climate Action</br></br></h5>
+                <p><b>UN SDG 13</b></p>
+                <p>Take urgent action to combat climate change and its impacts</p>
+              </li>
             </ul>
           </div>
+
         </div>
       </div>
-
-      <div class="template-align-center">
-        <!-- Button -->
-        <a href="https://dreamspace.academy" class="template-component-button template-component-button-style-1">This page is under construction<i></i></a>
-      </div>
-
     </div>
-
   </div>
-
 </div>
 
 <!-- Footer -->
