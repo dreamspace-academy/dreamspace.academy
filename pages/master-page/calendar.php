0ec787c292ba570e9dce130b0a8ec8063c8ac691
Aravinth Panch <me@aravinth.info>
2020-03-02 11:35:00 +0530

    Update calendar to view in daily mode

--- pages/master-page/calendar.php
+++ pages/master-page/calendar.php
@@ -10,7 +10,7 @@
       <div></div>
     </div>
 
-    <iframe src="https://calendar.google.com/calendar/b/4/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FColombo&amp;src=ZHJlYW1zcGFjZS5hY2FkZW15X3ExdTg3cnFjdGZxdG9mMXJ0c2JxNnBta204QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%233f51b5&amp;showTitle=0&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;mode=WEEK"
+    <iframe src="https://calendar.google.com/calendar/b/4/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FColombo&amp;src=ZHJlYW1zcGFjZS5hY2FkZW15X3ExdTg3cnFjdGZxdG9mMXJ0c2JxNnBta204QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%233f51b5&amp;showTitle=0&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;mode=AGENDA"
       style="border-width:0" width="1080" height="600" frameborder="0" scrolling="no"></iframe>
     </center>
 
