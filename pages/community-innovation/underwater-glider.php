7332822aef081a231ded131f164c4dbf2d459004
Aravinth Panch <me@aravinth.info>
2020-03-02 23:53:17 +0530

    Fix underwater glider content

--- pages/community-innovation/underwater-glider.php
+++ pages/community-innovation/underwater-glider.php
@@ -70,16 +70,15 @@
             Sri Lanka is an island country, located in the Indian Ocean, with a coastline of 1,585 km long, and the country claims an Exclusive Economic Zone (EEZ) extending 200 nautical miles, which is approximately 6.7 times Sri Lanka's land area.
           </p>
           <p class="template-padding-reset" align="justify">
-            The coastline and adjacent waters support highly productive marine ecosystems provide a massive economic value to this small nation, while this unique geographical position is highly contested by global powers.
+            The coastline and adjacent waters support highly productive marine ecosystems, and provide a massive economic value to this small nation, while this unique geographical position is highly contested by global powers.
           </p>
           <a target="_blank" href="http://www.ft.lk/article/620407/Oceans-and-Sri-Lanka-s-future--Towards-a-blue-economy">[more]</a>
           <h6 class="template-margin-top-3">Life Below Water</h6>
           <p class="template-padding-reset" align="justify">
-            The world’s oceans – their temperature, chemistry, currents and life – drive global systems that make the Earth habitable for humankind. How we manage this vital resource is essential for humanity as a whole, and to counterbalance the
-            effects of climate change.
+            The world’s waters – their temperature, chemistry, currents and life – drive global systems that make the Earth habitable. How we manage this vital resource is essential for life on earth, and to counterbalance the effects of climate change.
           </p>
           <p class="template-padding-reset" align="justify">
-            DreamSpace Academy adopts the sustainable development goals (UN SDG) to sustainably manage and protect marine and coastal ecosystems from pollution, as well as address the impacts of ocean acidification.
+            DreamSpace Academy has also adopted the sustainable development goal 14 “Life Below Water” to conserve and sustainably use the oceans, seas and marine resources.
           </p>
           <a target="_blank" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals/goal-14-life-below-water.html">[more]</a>
 
@@ -90,21 +89,19 @@
             roads, in many parts of the country.
           </p>
           <p class="template-padding-reset" align="justify">
-            There are no modern devices in Sri Lanka to measure and monitor the water ecosystem. Such equipment is very crucial for scientific research and assessment on an ecosystem, which is swiftly changing due to various weather conditions.
+            Therefore it is understandable that there is no modern infrastructure in Sri Lanka to measure and monitor the water ecosystems. Such equipment is very crucial for scientific research and assessment on an ecosystem, which is swiftly changing due to various weather conditions.
           </p>
           <a target="_blank" href="https://ec.europa.eu/maritimeaffairs/policy/ocean-governance_en">[more]</a>
 
           <h6 class="template-margin-top-3">Affordability</h6>
           <p class="template-padding-reset" align="justify">
-            There are several solutions, such as Autonomous underwater vehicles (AUVs), to gather real-time oceanographic data for an extended duration and distance with very little human effort. However, such commercial solutions could cost around
-            200,000 € and needs very special expertise & cost to maintain and repair such systems.
+            There are several solutions, such as Autonomous underwater vehicles (AUVs), to gather real-time oceanographic data for an extended duration and distance, with very little human effort. However, such commercial solutions could cost around 200,000 € and needs very special expertise & cost to maintain and repair such systems. Thus such solutions are not suitable for Sri Lanka.
           </p>
 
           <h4 class="template-margin-top-3">Solution</h4>
           <h6 class="template-margin-top-3">Open-source Underwater Glider (OSUG)</h6>
           <p class="template-padding-reset" align="justify">
-            There has been a breakthrough with low cost autonomous underwater vehicles and as this capability has matured a wide range of hobby and commercial applications have developed. There are no affordable extended duration underwater
-            exploration platforms and this project aims to address this need.
+            There has been a breakthrough with low cost autonomous underwater vehicles, and as this capability has matured a wide range of hobby and commercial applications have been developed. However, there are no affordable extended duration underwater exploration platforms and this project aims to address this need.
           </p>
           <p class="template-padding-reset" align="justify">
             <a target="_blank" href="https://www.linkedin.com/in/alexwxyz/">Alex Williams</a>, a young inventor from the USA, has developed a low cost and versatile underwater glider capable of extended missions of up to weeks at a time; using
@@ -122,17 +119,13 @@
 
           <h4 class="template-margin-top-3">Maker</h4>
           <p class="template-padding-reset" align="justify">
-            <a target="_blank" href="http://www.dailynews.lk/2019/09/12/tc/196709/walk-talk">Shanjeevan Amalanathan</a> is a young Maker from Batticaloa, Sri Lanka. To reinforce his innovative thinking, he joined DreamSpace Academy in 2019, and has
-            been developing the necessary Maker skills to develop Electronics, Mechanics and Softwares. After finishing his G.C.E Advanced Level in 2019, he decided to take a break, before he goes to University, to explore the ocean ecosystem in Sri
-            Lanka.
+            <a target="_blank" href="http://www.dailynews.lk/2019/09/12/tc/196709/walk-talk">Shanjeevan Amalanathan</a> is a young Maker from Batticaloa- a city on the eastern coast of Sri Lanka. To reinforce his innovative thinking, he joined DreamSpace Academy in 2019, and has been gaining the necessary Maker skills to develop Electronics, Mechanics and Softwares. After finishing his G.C.E Advanced Level in 2019, he decided to take a break, before he goes to University, to explore the ocean ecosystem in Sri Lanka.
           </p>
           <p class="template-padding-reset" align="justify">
-            During his study, he found out that after the Tsunami disaster in 2004, the ocean ecosystem in Sri Lanka was significantly disrupted, and the local community started seeing a large number of eels that had emerged from the backwaters in
-            Batticaloa, which locals presumed to be snakes, as such species were not commonly visible above water.
+            During his study, he found out that after the Tsunami disaster in 2004, the water ecosystem in Batticaloa was significantly disrupted, and the local community started seeing a large number of eels that had emerged from the backwaters, which locals presumed to be snakes, as such species were not commonly visible above water.
           </p>
           <p class="template-padding-reset" align="justify">
-            He saw the need for a system to monitor the ocean and backwaters around Batticaloa - a city on the eastern coast of Sri Lanka. Therefore he has taken the challenge to build an affordable underwater glider, with the help of the existing
-            open-source project OSUG.
+            He saw the need for a system to monitor the ocean and backwaters around Batticaloa. Therefore he has taken the challenge to build an affordable underwater glider, with the help of the existing open-source project OSUG.
           </p>
           <p class="template-padding-reset" align="justify">
             He strongly believes that he will be further contributing to the OSUG project, by making it a production-ready underwater glider with multiparameter measurements system that will play a vital role in protecting the Blue Ecosystems in
