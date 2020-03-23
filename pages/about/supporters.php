a3742f424cacb746ce06217cfb8a015f4fedbf27
Aravinth Panch <me@aravinth.info>
2020-03-23 20:06:15 +0100

    Update supporters

--- pages/about/supporters.php
+++ pages/about/supporters.php
@@ -1,136 +1,138 @@
-<!-- Section -->
-<div class="template-content-section template-padding-top-reset template-padding-bottom-5 template-main" id="supporters">
+<!-- Content -->
+<div class="template-content">
 
-	<!-- Header and subheader -->
-	<div class="template-component-header-subheader">
-		<h2>Our Supporters</h2>
-		<h6></h6>
-		<div></div>
-	</div>
+  <!-- Header and subheader -->
+  <div class="template-component-header-subheader">
+    <h2>Our Supporters</h2>
+    <h6></h6>
+    <div></div>
+  </div>
 
-	<!-- Layout 50x50 -->
-	<div class="template-layout-50x50 template-clear-fix">
+  <!-- Section -->
+  <div class="template-content-section template-padding-bottom-5 template-main">
 
-		<!-- Left column -->
-		<div class="template-layout-column-left">
-			<!--<h4>Our staff consists of three teachers and one assistant with experience in early childhood care.</h4>
-					</div>
-
-					<!-- Right column -->
-			<div class="template-layout-column-right">
-				<!--<p>Praesent arcu gravida vehicula est node maecenas loareet morbi a dosis luctus. Urna eget lacinia eleifend praesent luctus a arcu quis facilisis venenatis. Aenean interdum, nibh vitae sodales, magna ante feugiat elit maecenas.</p> -->
-			</div>
-
-		</div>
-
-		<!-- Team -->
-		<div class="template-component-team template-component-team-style-2">
-			<ul class="template-layout-50x50 template-clear-fix">
-				<li class="template-layout-column-left">
-					<ul class="template-layout-50x50 template-clear-fix">
-						<li class="template-layout-column-left">
-							<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-								<a href="../../media/images/about/supporters/maite-dreamspace.png" data-fancybox-group="team-2">
-									<img src="../../media/images/about/supporters/maite-dreamspace.png" alt="maite-growth-strategist-dreamspace" />
-									<span><span><span></span></span></span>
-								</a>
-								<div>
-									<h6>Maite Hernandez Zubeldia</h6>
-									<span>Advisor & Trainer - Design Thinking</span>
-								</div>
-								<p><b>Maite Hernandez Zubeldia</b> Advisor & Trainer - Design Thinking</p>
-							</div>
-						</li>
-						<li class="template-layout-column-right">
-							<div class="template-component-team-quote"></div>
-							<p class="template-component-team-description" style="text-align: justify">Maite’s core experience is in impact-driven entrepreneurship, human-centric design and agile product development. She has a strong passion for bridging the gap between humanity and technology. While working in Europe, Canada, Cuba and Sri Lanka, she has experienced innovation approaches in different regions of the world.
-							<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-								<ul>
-									<li><a href="https://www.linkedin.com/in/maitezubeldia/" class="template-component-social-icon-linkedin"></a></li>
-								</ul>
-							</div>
-						</li>
-					</ul>
-				</li>
-				<li class="template-layout-column-right">
-				  <ul class="template-layout-50x50 template-clear-fix">
-				    <li class="template-layout-column-left">
-				      <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-				        <a href="../../media/images/about/supporters/darina-dreamspace.png" data-fancybox-group="team-2">
-				          <img src="../../media/images/about/supporters/darina-dreamspace.png" alt="darina-dreamspace.png" />
-				          <span><span><span></span></span></span>
-				        </a>
-				        <div>
-				          <h6>Darina Onoprienko</h6>
-				          <span>Advisor & Trainer - Entrepreneurship</span>
-				        </div>
-				        <p><b>Darina Onoprienko</b> Advisor & Trainer - Entrepreneurship</p>
-				      </div>
-				    </li>
-				    <li class="template-layout-column-right">
-				      <div class="template-component-team-quote"></div>
-				      <p class="template-component-team-description" style="text-align: justify">The areas of my professional interest are Internet of Things, blockchain, cleantech, sustainable transportation and supply chain management, design thinking and innovation management, entrepreneurship. </p>
-				      <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-				        <ul>
-				          <li><a href="https://www.linkedin.com/in/darina-onoprienko-6b5b8843/" class="template-component-social-icon-linkedin"></a></li>
-				        </ul>
-				      </div>
-				    </li>
-				  </ul>
-				</li>
-				<li class="template-layout-column-left">
-				  <ul class="template-layout-50x50 template-clear-fix">
-				    <li class="template-layout-column-left">
-				      <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-				        <a href="../../media/images/about/supporters/luise-dreamspace.png" data-fancybox-group="team-2">
-				          <img src="../../media/images/about/supporters/luise-dreamspace.png" alt="luise-dreamspace.png" />
-				          <span><span><span></span></span></span>
-				        </a>
-				        <div>
-				          <h6>Luise Kröning</h6>
-				          <span>Design Thinking Coach & Innovation Consultant</span>
-				        </div>
-				        <p><b>Darina Onoprienko</b> Design Thinking Coach & Innovation Consultant</p>
-				      </div>
-				    </li>
-				    <li class="template-layout-column-right">
-				      <div class="template-component-team-quote"></div>
-				      <p class="template-component-team-description" style="text-align: justify">Creating a mindful movement by supporting people to unfold their potential through creativity and connection. Passionate about empowering social entrepreneurs and fostering social innovations in Europe and Asia.</p>
-				      <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-				        <ul>
-				          <li><a href="https://www.linkedin.com/in/luise-the-creative-strategist/" class="template-component-social-icon-linkedin"></a></li>
-				        </ul>
-				      </div>
-				    </li>
-				  </ul>
-				</li>
-				<li class="template-layout-column-right">
-				  <ul class="template-layout-50x50 template-clear-fix">
-				    <li class="template-layout-column-left">
-				      <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-				        <a href="../../media/images/about/supporters/zoe-dreamspace.png" data-fancybox-group="team-2">
-				          <img src="../../media/images/about/supporters/zoe-dreamspace.png" alt="zoe-dreamspace.png" />
-				          <span><span><span></span></span></span>
-				        </a>
-				        <div>
-				          <h6>Zoë Harman Conlon</h6>
-				          <span>Community & Event Manager</span>
-				        </div>
-				        <p><b>Darina Onoprienko</b> Advisor & Trainer - Community Building</p>
-				      </div>
-				    </li>
-				    <li class="template-layout-column-right">
-				      <div class="template-component-team-quote"></div>
-				      <p class="template-component-team-description" style="text-align: justify">Passionate about sustainability, social entrepreneurship and community mobilization focused on climate action in Europe and Asia.</p>
-				      <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-				        <ul>
-				          <li><a href="https://www.linkedin.com/in/zo%C3%AB-harman-conlon-85986880/" class="template-component-social-icon-linkedin"></a></li>
-				        </ul>
-				      </div>
-				    </li>
-				  </ul>
-				</li>
-			</ul>
-		</div>
-
-	</div>
+    <!-- Gallery -->
+    <div class="template-component-gallery">
+      <ul class="template-layout-25x25x25x25 template-clear-fix">
+        <li class="template-layout-column-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/maite-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Maite Hernandez Zubeldia</h6>
+              </br>
+              <span>Design Thinking Trainer</span>
+              <span>Germany | Cuba</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/maitezubeldia/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/darina-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Darina Onoprienko</h6>
+              </br>
+              </br>
+              <span>Entrepreneurship Trainer</span>
+              <span>Germany | Russia</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/darina-onoprienko-6b5b8843/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/nico-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Dr. Nico Meißner</h6>
+              </br>
+              </br>
+              <span>Storytelling Trainer</span>
+              <span>Australia | Malaysia</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/nicomei%C3%9Fner/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/luise-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Luise Kröning</h6>
+              </br>
+              </br>
+              <span>Design Thinking Coach</span>
+              <span>Germany</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/luise-the-creative-strategist/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/zoe-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Zoë Harman Conlon</h6>
+              </br>
+              </br>
+              <span>Community Manager</span>
+              <span>Germany | Ireland</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/zo%C3%AB-harman-conlon-85986880/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/melani-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Melani Jayamanna</h6>
+              </br>
+              </br>
+              <span>Business Development</span>
+              <span>Germany | Italy</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/melanijayamanna/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/about/supporters/soniya-dreamspace.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Soniya Siva</h6>
+              </br>
+              </br>
+              <span>Business Development</span>
+              <span>Canada | China</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/soniyasiva/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
