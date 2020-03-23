1143960a06c96a7f6a41bebb1a41a03b33cac93b
Aravinth Panch <me@aravinth.info>
2020-03-23 23:00:56 +0100

    Merge supporters to the team

--- pages/about/index.php
+++ pages/about/index.php
@@ -7,11 +7,6 @@
 		<?php include('team.php') ?>
 	</section>
 
-	<!-- supporters-section -->
-	<section id="supporters" >
-				<?php include('supporters.php') ?>
-			</section>
-
 	<!-- partners-section -->
 	<section id="partners" style="margin-bottom: 100px;">
 		<?php include('partners.php') ?>
