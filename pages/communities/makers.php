ba9ad4602a95c980f94713769d1139e2397c0aef
Viththiyakaran <battikaran6@gmail.com>
2020-04-24 12:02:27 +0530

    Add priyan and amir into makerspage

--- pages/communities/makers.php
+++ pages/communities/makers.php
@@ -166,7 +166,7 @@
         </li>
         <li class="template-layout-column-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../../media/images/communities/makers/thanajayan-dreamspace-makers.jpg" alt="dreamspace-makers" />
+            <img src="../../media/images/communities/makers/thanajayan-dreamspace-junior-makers.jpg" alt="dreamspace-makers" />
             <div>
               <h6>Thananjayan</h6>
             </br><span>Innovator</span>
@@ -180,6 +180,39 @@
             </div>
           </div>
         </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/communities/makers/amirkhan-dreamspace-junior-makers.jpg" alt="dreamspace-makers" />
+            <div>
+              <h6>Nazsar Imanuval Ameerkhan </h6>
+              </br><span>Innovator</span>
+            <span>Batticaloa</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/company/dreamspace-academy" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/siva.mayuran.148" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../../media/images/communities/makers/pathmapriyan-dreamspace-junior-makers.jpg" alt="dreamspace-makers" />
+            <div>
+              <h6>Murugupillai Pathmapriyan</h6>
+            </br><span>Innovator</span>
+            <span>Batticaloa</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/company/dreamspace-academy" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/pathma.priyan.7" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        
         <!-- <li class="template-layout-column-right">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../../media/images/communities/makers/aravinth-dreamspace-makers.jpg" alt="dreamspace-makers" />
