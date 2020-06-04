1bc67c7df4c9fd530ea27c94d1072dacc5dcbaee
Aravinth Panch <me@aravinth.info>
2020-06-04 23:11:46 +0200

    Fix broken links

--- pages/3-1-software-lab.php
+++ pages/3-1-software-lab.php
@@ -14,10 +14,10 @@
 			<!-- Nivo slider -->
 			<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
 				<div>
-					<img src="../media/images/org-labs/lab-software-dreamspace-software-lab-2.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-software-lab-2.jpg" alt="maker-education-dreamspace-academy" />
-					<img src="../media/images/org-labs/lab-software-dreamspace-software-lab-3.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-software-lab-3.jpg" alt="maker-education-dreamspace-academy" />
-					<img src="../media/images/org-labs/lab-software-dreamspace-software-lab-4.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-software-lab-4.jpg" alt="maker-education-dreamspace-academy" />
-					<img src="../media/images/org-labs/lab-software-dreamspace-software-lab-5.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-software-lab-5.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../media/images/org-labs/lab-software-dreamspace-2.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-2.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../media/images/org-labs/lab-software-dreamspace-3.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-3.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../media/images/org-labs/lab-software-dreamspace-4.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-4.jpg" alt="maker-education-dreamspace-academy" />
+					<img src="../media/images/org-labs/lab-software-dreamspace-5.jpg" data-thumb="../media/images/org-labs/lab-software-dreamspace-5.jpg" alt="maker-education-dreamspace-academy" />
 				</div>
 			</div>
 		</div>
