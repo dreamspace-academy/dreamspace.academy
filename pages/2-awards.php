06b255c114e2c11d600d8a3dd6099c042d387023
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-09-10 19:37:29 +0530

    Update team details and add new members

--- pages/2-awards.php
+++ pages/2-awards.php
@@ -1,6 +1,6 @@
 
 <!-- Content -->
-<div class="template-content" id="awards"> 
+<div class="template-content" id="awards">
 
   <!-- Header and subheader -->
   <div class="template-component-header-subheader">
@@ -10,13 +10,41 @@
   </div>
 
   <!-- Section -->
-  <div class="template-content-section template-padding-bottom-5 template-main">
+  <div class="template-content-section template-padding-bottom-5 template-background-color-2" id="chess">
+  	<div class="template-main">
+  		<div class="template-layout-50x50 template-clear-fix">
+  			<div class="template-layout-column-left template-margin-bottom-reset">
+  				<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+  					<div>
+  						<img src="../media/images/community-chess/chess-dreamspace-1.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-1.jpg" alt="Chess Community" />
+  						<img src="../media/images/community-chess/chess-dreamspace-2.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-2.jpg" alt="Chess Community" />
+  						<img src="../media/images/community-chess/chess-dreamspace-3.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-3.jpg" alt="Chess Community" />
+  					</div>
+  				</div>
+  			</div>
+  			<div class="template-layout-column-right template-margin-bottom-reset">
+  				<div class="template-align-center">
+  					<h2>Chess</h2>
+  					<div class="template-component-divider template-component-divider-style-2"></div>
+  					<div class="template-component-italic template-margin-top-3">
+  						Our Chess community is actively training the young minds to grasp the intricacies of the game
+  					</div>
+  					<p class="template-margin-top-3">
+  						It is widely accepted by most historians that the game of chess originated in India in the fifth or sixth century AD.
+  					</p>
+  					<p class="template-margin-top-3">
+  						The earliest known form of chess is two-handed chaturanga, Sanskrit for “the four branches of the army.” Like real Indian armies at that time, the pieces were called elephants, chariots, horses, and foot soldiers. Unlike modern chess, chaturanga was mainly a game of chance; results depended on how well you rolled the dice.
+  					</p>
+  					<p class="template-margin-top-3">
+  						From India, chaturanga spread quickly to Persia, where it was called chatrang. When Arabs invaded Persia in the seventh century, they called it shatranj and popularized it throughout the Arab world. Chess made its way to Europe in the tenth century as a result of the Arab expansion.
+  					</p>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  </div>
 
-    <!-- Gallery -->
-    <div class="template-component-gallery">
-      <ul class="template-layout-25x25x25x25 template-clear-fix">
-        <!-- Supporters -->
-        <li class="template-layout-column-left">
+        <!-- Supporters <li class="template-layout-column-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../media/images/org-supporters/supporters-dreamspace-maite.jpg" alt="dreamspace-supporters" />
             <div>
--- pages/2-team.php
+++ pages/2-team.php
@@ -38,7 +38,8 @@
             <div>
               <h6>Aravinth Panch</h6>
               </br>
-              <span>Co-founder & Director of Innovation</span>
+              <span>Co-founder & Director
+Guardian of Guardians</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
@@ -49,6 +50,23 @@
           </div>
         </li>
         <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-sarri.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Sarri Bater</h6>
+              </br>
+              <span>Guardian of</span>
+                <span>Strategies and Planning</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.openedge.org.uk/sarri-bater.html" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-right">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../media/images/org-team/team-dreamspace-jayanthan.jpg" alt="dreamspace-team" />
             <div>
@@ -64,7 +82,7 @@
             </div>
           </div>
         </li>
-        <li class="template-layout-column-right">
+        <li class="template-layout-column-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../media/images/org-team/team-dreamspace-shanjeevan.jpg" alt="dreamspace-team" />
             <div>
@@ -80,91 +98,6 @@
             </div>
           </div>
         </li>
-        <li class="template-layout-column-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-team/team-dreamspace-suhail.jpg" alt="dreamspace-team" />
-            <div>
-              <h6>Suhail Mohammed</h6>
-              </br>
-              <span>Guardian of Bio Lab</span>
-              </br>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
-                <li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-center-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-team/team-dreamspace-grason.jpg" alt="dreamspace-team" />
-            <div>
-              <h6>Grason Prashanth</h6>
-              </br>
-              <span>Guardian of Art Lab</span>
-              </br>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/grasonprashanth/" class="template-component-social-icon-linkedin"></a></li>
-                <li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-center-right">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-team/team-dreamspace-sarri.jpg" alt="dreamspace-team" />
-            <div>
-              <h6>Sarri Bater</h6>
-              </br>
-              <span>Guardian of Strategies</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
-                <li><a target="_blank" href="https://www.openedge.org.uk/sarri-bater.html" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-right">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-team/team-dreamspace-sajani.jpg" alt="dreamspace-team" />
-            <div>
-              <h6>Sajani Balasingam</h6>
-              </br>
-              <span>Guardian of</span>
-              <span>Diversity & Inclusion</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
-                <li><a target="_blank" href="https://www.facebook.com/saja.sai/" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-left">
-
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-team/team-dreamspace-anna.jpg" alt="dreamspace-team" />
-            <div>
-              <h6>Anna Jeyaraj Moses</h6>
-              </br>
-              <span>Guardian of</span>
-              <span>Social Impact</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
-                <li><a target="_blank" href="https://www.facebook.com/Annie.Moses.J" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
         <li class="template-layout-column-center-left">
 
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
@@ -173,6 +106,7 @@
               <h6>Charuni Indrajith</h6>
               </br>
               <span>Guardian of Operations</span>
+              </br>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
@@ -201,6 +135,75 @@
           </div>
         </li>
         <li class="template-layout-column-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-sajani.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Sajani Balasingam</h6>
+              </br>
+              <span>Guardian of</span>
+              <span>Diversity & Inclusion</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/saja.sai/" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-left">
+
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-basith.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Abdul Basith</h6>
+              </br>
+              <span>Guardian of</span>
+              <span>Perfomance Marketing</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/abdul-basith-rafeekin-9a7334192/" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/profile.php?id=100012693817746" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-grason.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Grason Prashanth</h6>
+              </br>
+              <span>Guardian of Art Lab</span>
+              </br>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/grasonprashanth/" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-suhail.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Suhail Mohammed</h6>
+              </br>
+              <span>Guardian of Bio Lab</span>
+              </br>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-right">
 
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../media/images/org-team/team-dreamspace-ahaliya.jpg" alt="dreamspace-team" />
@@ -209,6 +212,42 @@
               </br>
               <span>Guardian of</span>
               <span>Diversity & Inclusion</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-anna.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Anna Jeyaraj Moses</h6>
+              </br>
+              <span>Guardian of</span>
+              <span>Social Impact</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/Annie.Moses.J" class="template-component-social-icon-rss"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-team/team-dreamspace-zeena.jpg" alt="dreamspace-team" />
+            <div>
+              <h6>Zeena Nazeer</h6>
+              </br>
+              <span>Guardian of</span>
+              <span>Micro Art</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
