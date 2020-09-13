87db09f08c7e5bc779041925ba1a05f322bc8a42
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-09-13 17:06:06 +0530

    Update Awards

--- pages/2-awards.php
+++ pages/2-awards.php
@@ -15,7 +15,43 @@
     <!-- Gallery -->
     <div class="template-component-gallery">
       <ul class="template-layout-25x25x25x25 template-clear-fix">
+
         <li class="template-layout-column-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-awards/awards-dreamspace-global-community-bio-fellowship.jpg" alt="dreamspace-supporters" />
+            <div>
+              <h6>Global Community Bio Fellowship</h6>
+              </br>
+              </br>
+              <span>Global Bio Summit 4.0</span>
+              <span>2020</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.biosummit.org/fellows" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+
+        <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/images/org-awards/awards-dreamspace-genu.jpg" alt="dreamspace-supporters" />
+            <div>
+              <h6>Generation Unlimited Youth Challenge</h6>
+              </br>
+              </br>
+              <span>Gen U</span>
+              <span>2020</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+              <li><a target="_blank" href="https://www.generationunlimited.org/" class="template-component-social-icon-linkedin"></a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+          <li class="template-layout-column-center-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
             <img src="../media/images/org-awards/awards-dreamspace-falling-walls.jpg" alt="dreamspace-supporters" />
             <div>
@@ -31,62 +67,28 @@
             </div>
           </div>
         </li>
-        <li class="template-layout-column-center-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-awards/awards-dreamspace-hackdev.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Hackdev Social Innovation Challenge</h6>
-              </br>
-              </br>
-              <span>Hackdev</span>
-              <span>2019</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.hackadev.lk/" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-center-right">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-awards/awards-dreamspace-genu.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Generation Unlimited Youth Challenge</h6>
-              </br>
-              </br>
-              <span>Gen U</span>
-              <span>2020</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-              <li><a target="_blank" href="https://www.generationunlimited.org/" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
+
         <li class="template-layout-column-right">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-awards/awards-dreamspace-global-community-bio-fellowship.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Global Community Bio Fellowship</h6>
-              </br>
-              </br>
-              <span>Global Bio Summit 4.0</span>
-              <span>2020</span>
+            <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+              <img src="../media/images/org-awards/awards-dreamspace-hackdev.jpg" alt="dreamspace-supporters" />
+              <div>
+                <h6>Hackdev Social Innovation Challenge</h6>
+                </br>
+                </br>
+                <span>Hackdev</span>
+                <span>2019</span>
+              </div>
+              <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+                <ul>
+                  <li><a target="_blank" href="https://www.hackadev.lk/" class="template-component-social-icon-linkedin"></a></li>
+                </ul>
+              </div>
             </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.biosummit.org/fellows" class="template-component-social-icon-rss"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
+          </li>
+
               </ul>
             </div>
           </div>
         </li>
       </ul>
     </div>
-  </div>
-</div>
