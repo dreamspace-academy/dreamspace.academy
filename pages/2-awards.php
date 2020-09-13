3e6ef65ba44d501b406742e892aabac3602bd624
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-09-13 16:36:29 +0530

    Add awards detail and images

--- pages/2-awards.php
+++ pages/2-awards.php
@@ -10,158 +10,82 @@
   </div>
 
   <!-- Section -->
-  <div class="template-content-section template-padding-bottom-5 template-background-color-2" id="chess">
-  	<div class="template-main">
-  		<div class="template-layout-50x50 template-clear-fix">
-  			<div class="template-layout-column-left template-margin-bottom-reset">
-  				<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
-  					<div>
-  						<img src="../media/images/community-chess/chess-dreamspace-1.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-1.jpg" alt="Chess Community" />
-  						<img src="../media/images/community-chess/chess-dreamspace-2.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-2.jpg" alt="Chess Community" />
-  						<img src="../media/images/community-chess/chess-dreamspace-3.jpg" data-thumb="../media/images/community-chess/chess-dreamspace-3.jpg" alt="Chess Community" />
-  					</div>
-  				</div>
-  			</div>
-  			<div class="template-layout-column-right template-margin-bottom-reset">
-  				<div class="template-align-center">
-  					<h2>Chess</h2>
-  					<div class="template-component-divider template-component-divider-style-2"></div>
-  					<div class="template-component-italic template-margin-top-3">
-  						Our Chess community is actively training the young minds to grasp the intricacies of the game
-  					</div>
-  					<p class="template-margin-top-3">
-  						It is widely accepted by most historians that the game of chess originated in India in the fifth or sixth century AD.
-  					</p>
-  					<p class="template-margin-top-3">
-  						The earliest known form of chess is two-handed chaturanga, Sanskrit for “the four branches of the army.” Like real Indian armies at that time, the pieces were called elephants, chariots, horses, and foot soldiers. Unlike modern chess, chaturanga was mainly a game of chance; results depended on how well you rolled the dice.
-  					</p>
-  					<p class="template-margin-top-3">
-  						From India, chaturanga spread quickly to Persia, where it was called chatrang. When Arabs invaded Persia in the seventh century, they called it shatranj and popularized it throughout the Arab world. Chess made its way to Europe in the tenth century as a result of the Arab expansion.
-  					</p>
-  				</div>
-  			</div>
-  		</div>
-  	</div>
-  </div>
+  <div class="template-content-section template-padding-bottom-5 template-main">
 
-        <!-- Supporters <li class="template-layout-column-left">
+    <!-- Gallery -->
+    <div class="template-component-gallery">
+      <ul class="template-layout-25x25x25x25 template-clear-fix">
+        <li class="template-layout-column-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-maite.jpg" alt="dreamspace-supporters" />
+            <img src="../media/images/org-awards/awards-dreamspace-falling-walls.jpg" alt="dreamspace-supporters" />
             <div>
-              <h6>Maite Hernandez Zubeldia</h6>
+              <h6>Falling walls Sciece Engagement</h6>
               </br>
-              <span>Design Thinking Trainer</span>
-              <span>Germany | Cuba</span>
+              <span>Falling Walls Engage</span>
+              <span>2019</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/maitezubeldia/" class="template-component-social-icon-linkedin"></a></li>
+              <li><a target="_blank" href="https://www.globalinnovationgathering.org/2019/11/13/dreamspace-academy-won-the-falling-walls-science-engagement-award/" class="template-component-social-icon-rss"></a></li>
               </ul>
             </div>
           </div>
         </li>
         <li class="template-layout-column-center-left">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-darina.jpg" alt="dreamspace-supporters" />
+            <img src="../media/images/org-awards/awards-dreamspace-hackdev.jpg" alt="dreamspace-supporters" />
             <div>
-              <h6>Darina Onoprienko</h6>
+              <h6>Hackdev Social Innovation Challenge</h6>
               </br>
               </br>
-              <span>Entrepreneurship Trainer</span>
-              <span>Germany | Russia</span>
+              <span>Hackdev</span>
+              <span>2019</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/darina-onoprienko-6b5b8843/" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.hackadev.lk/" class="template-component-social-icon-rss"></a></li>
               </ul>
             </div>
           </div>
         </li>
         <li class="template-layout-column-center-right">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-nico.jpg" alt="dreamspace-supporters" />
+            <img src="../media/images/org-awards/awards-dreamspace-genu.jpg" alt="dreamspace-supporters" />
             <div>
-              <h6>Dr. Nico Meißner</h6>
+              <h6>Generation Unlimited Youth Challenge</h6>
               </br>
               </br>
-              <span>Storytelling Trainer</span>
-              <span>Australia | Malaysia</span>
+              <span>Gen U</span>
+              <span>2020</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/nicomei%C3%9Fner/" class="template-component-social-icon-linkedin"></a></li>
+              <li><a target="_blank" href="https://www.generationunlimited.org/" class="template-component-social-icon-rss"></a></li>
               </ul>
             </div>
           </div>
         </li>
         <li class="template-layout-column-right">
           <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-luise.jpg" alt="dreamspace-supporters" />
+            <img src="../media/images/org-awards/awards-dreamspace-global-community-bio-fellowship.jpg" alt="dreamspace-supporters" />
             <div>
-              <h6>Luise Kröning</h6>
+              <h6>Global Community Bio Fellowship</h6>
               </br>
               </br>
-              <span>Innovation Consultant</span>
-              <span>Germany</span>
+              <span>Global Bio Summit 4.0</span>
+              <span>2020</span>
             </div>
             <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
               <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/luise-the-creative-strategist/" class="template-component-social-icon-linkedin"></a></li>
+                <li><a target="_blank" href="https://www.biosummit.org/fellows" class="template-component-social-icon-rss"></a></li>
               </ul>
             </div>
           </div>
         </li>
-        <li class="template-layout-column-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-zoe.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Zoë Harman Conlon</h6>
-              </br>
-              </br>
-              <span>Event Manager</span>
-              <span>Germany | Ireland</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/zo%C3%AB-harman-conlon-85986880/" class="template-component-social-icon-linkedin"></a></li>
               </ul>
             </div>
           </div>
         </li>
-        <!-- <li class="template-layout-column-center-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-melani.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Melani Jayamanna</h6>
-              </br>
-              </br>
-              <span>Business Development</span>
-              <span>Germany | Italy</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/melanijayamanna/" class="template-component-social-icon-linkedin"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li> -->
-        <!-- <li class="template-layout-column-center-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/images/org-supporters/supporters-dreamspace-soniya.jpg" alt="dreamspace-supporters" />
-            <div>
-              <h6>Soniya Siva</h6>
-              </br>
-              </br>
-              <span>Business Development</span>
-              <span>Canada | China</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/soniyasiva/" class="template-component-social-icon-linkedin"></a></li>
-              </ul>
-            </div>
-          </div>
-        </li> -->
       </ul>
     </div>
   </div>
