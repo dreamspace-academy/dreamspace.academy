c47ffd7edf0720508ea692599903aaac1b26cb19
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-10-12 15:53:26 +0530

    Update circular plastic content

--- pages/3-2-circular-plastic.php
+++ pages/3-2-circular-plastic.php
@@ -46,30 +46,56 @@
       <div class="template-post-section-content">
         <div class="template-post-content">
           <h4 class="template-margin-top-3">Motivation</h4>
-          <h6 class="template-margin-top-3">Blue Economy</h6>
+          <h6 class="template-margin-top-3">Sri Lanka: A great ocean polluter with plastics</h6>
           <p class="template-padding-reset" align="justify">
-            Sri Lanka is an island country, located in the Indian Ocean, with a coastline of 1,585 km long, and the country claims an Exclusive Economic Zone (EEZ) extending 200 nautical miles, which is approximately 6.7 times Sri Lanka's land area.
+            According to the earthday.org, Sri Lanka is placed as the 5th largest plastic polluter in the world among countries such as China, Indonesia, the Philippines, and Vietnam. We waste more than 5 Million kg of plastic, per day, despite not being one of the world’s largest consumers of plastic.
+
           </p>
           <p class="template-padding-reset" align="justify">
-            The coastline and adjacent waters support highly productive marine ecosystems, and provide a massive economic value to this small nation, while this unique geographical position is highly contested by global powers.
+            For many people in Sri Lanka, we’re just happy to litter wherever we are and with no concern about the environment or potential impacts to wildlife. You only need to walk on a beach or see the aftermath of a party or other public gathering in Sri Lanka to understand the scope of the problem.
           </p>
-          <a target="_blank" href="http://www.ft.lk/article/620407/Oceans-and-Sri-Lanka-s-future--Towards-a-blue-economy">[more]</a>
-          <h6 class="template-margin-top-3">Life Below Water</h6>
+          <a target="_blank" href="https://loopsagency.com/sri-lanka-ranked-as-the-5th-largest-plastic-polluter-in-the-world/">[more]</a>
+          <h6 class="template-margin-top-3">Marine diversity in Sri Lanka</h6>
           <p class="template-padding-reset" align="justify">
-            The world’s waters – their temperature, chemistry, currents and life – drive global systems that make the Earth habitable. How we manage this vital resource is essential for life on earth, and to counterbalance the effects of climate change.
-          </p>
+            Sri Lanka, as an island nation with a coastline of 1,585 kilometres,is home to a rich and diverse marine life. But little is known about the vast array of species that inhabit our waters or about the marine habitats that these species inhabit.
+         </p>
           <p class="template-padding-reset" align="justify">
-            DreamSpace Academy has also adopted the sustainable development goal 14 “Life Below Water” to conserve and sustainably use the oceans, seas and marine resources.
+          Through this calendar, IUCN Sri Lanka particularly aims to create a better understanding and awareness on the importance and threats facing a selection of Sri Lanka’s marine species.
           </p>
-          <a target="_blank" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals/goal-14-life-below-water.html">[more]</a>
+          <a target="_blank" href="https://window2nature.wordpress.com/2013/01/05/the-sundaytimes-sri-lanka-icun-2013-desk-calendar-features-marine-wonders/">[more]</a>
 
           <h4 class="template-margin-top-3">Problem</h4>
-          <h6 class="template-margin-top-3">Plastic Waste</h6>
+          <h6 class="template-margin-top-3">Ocean Water Contamination</h6>
+          <p class="template-padding-reset" align="justify">
+            Plastic Waste is a massive problem. The Ellen MacArthur Foundation predicts that by 2040 there will be more plastic in our oceans than fish. Plastic waste is causing toxins to enter our ecosystems - damaging humans and animals. Unfortunately, many people are not aware of the plastic waste problem, and when they are aware of it, they feel powerless about what to do about it. We want to increase public awareness about the plastic waste problem by turning post-consumer plastic into cool durable products.
+          </p>
+          <h6 class="template-margin-top-3">Solid Waste Management</h6>
+          <p class="template-padding-reset" align="justify">
+            Waste management has become a huge challenge for countries worldwide due to the vast amounts of waste produced from modern consumption. Unfortunately, most of this waste ends up in landfills, burned, or even worse - in the environment or the ocean. Unfortunately, the use of plastic in single-use packaging has significantly increased over the last 20 years. Plastic waste is especially an issue because of the long time it takes to degrade. The construction industry is known to be one of the biggest producers of landfilled waste. According to the Journal of Construction Engineering and Management, approximately 26% of landfills are occupied with construction waste. The question is - how can we push the construction and design industry to move towards more sustainable options and solutions?
+          </p>
 
           <h4 class="template-margin-top-3">Solution</h4>
           <h6 class="template-margin-top-3">Circular Plastic</h6>
+          <p class="template-padding-reset" align="justify">
+            Precious Plastic shows the world an incredible opportunity of plastic waste in order to eliminate plastic pollution, reducing the demand for new virgin plastic and closing its materials loop while creating better livelihoods for people around the world. Precious Plastic is a cultural tool to change the way society perceives plastic. All plastic laying around you is a resource, not waste. It is a material with great potential, left intact all around the world and if processed correctly, can become something new and valuable. This resource can become a source of income or an educational tool for your community, at the same time making sure it doesn’t enter our environment.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            They have created machines, documentation, video tutorials, campaigns and a platform to empower people across the planet to start working with plastic waste locally while trying to decentralise and localise plastic recycling technologies, infrastructures and knowledge by providing open-source designs for machines, moulds and consumer products, as well as business models and tools for you to set up and run a workspace.They share everything open source for anyone to benefit from. This means that all the knowledge, know-how, processes, methodologies and tools are available online, for free at all times. This open source philosophy is at the core of Precious Plastic and shapes most decisions within their team. They believe that collaboration is stronger than competition and that the plastic problem can only be solved collectively.
+          </p>
+          <p class="template-padding-reset" align="justify">
+            Circular Plastic collaborated with the precious plastic to develop and accommodate the open source system into the Sri Lanka local environment. We have a working community for this project from different backgrounds to develop multi disciplinary and dimension projects. We developed a Social business model to assure the sustainability of the project.
+          </p>
+
+        <a target="_blank" href="https://preciousplastic.com/index.html">[more]</a>
 
           <h4 class="template-margin-top-3">Maker</h4>
+          <p class="template-padding-reset" align="justify">
+            Jayanthan Amalanathan is a 23-year-old community worker. He is a mechatronic undergraduate of Uva Wellassa University Sri Lanka. He is working as a part-time employee at DreamSpace academy as a guardian of the mechanical and electronic lab. He is passionate about tackling socio, economical and environmental challenges with his expertise in Mechanical and Electronic engineering through Social Innovation and Social entrepreneurship.
+          </p>
+
+          <p class="template-padding-reset" align="justify">
+            He has done research related to Climate Change, Waste management, Water resource management and agriculture and published papers in National and International conferences. He presented "Optimizing effective use over the current practice of waste management in Srilanka and its impact on Climate Change" in the International Climate Change Conference (ICCC). He is a Gold awardee of Duke of Edinburgh International award programme. He won ASU Watson Sustainability Solution Awards of Arizona State University and the winner in the National Science Research project competition conducted by the national science foundation under the Ministry of Research and Technology.
+          </p>
 
           <!-- SDGs -->
           <h4 class="template-margin-top-5 template-margin-bottom-5">This innovation collects SDG Badges of</h4>
@@ -77,21 +103,21 @@
             <ul class="template-layout-33x33x33 template-clear-fix">
               <li class="template-layout-column-left">
                 <div class="template-icon-feature"><img src="../media/images/logos-sdg/un-sdg-dreamspace-12.png" alt="un-sdg-dreamspace" /></div>
-                <h5>Sustainable Consumption and Production</br></br></h5>
+                <h5>Sustainable Consumption and Production</br></h5>
                 <p><b>UN SDG 12</b></p>
-                <p>Ensure sustainable consumption and production patterns</p>
+                <p>Reduce waste generation through prevention, reduction, recycling and reuse</p>
               </li>
               <li class="template-layout-column-center">
                 <div class="template-icon-feature"><img src="../media/images/logos-sdg/un-sdg-dreamspace-14.png" alt="un-sdg-dreamspace" /></div>
-                <h5>Life Below Water</br></br></br></br></h5>
+                <h5>Life Below Water</br></br></br></h5>
                 <p><b>UN SDG 14</b></p>
-                <p>Conserve and sustainably use the oceans, seas and marine resources for sustainable development</p>
+                <p>Prevent and significantly reduce marine pollution of all kinds.</p>
               </li>
               <li class="template-layout-column-right">
                 <div class="template-icon-feature"><img src="../media/images/logos-sdg/un-sdg-dreamspace-11.png" alt="un-sdg-dreamspace" /></div>
-                <h5>Sustainable Cities and Communities</br></br></br></h5>
+                <h5>Sustainable Cities and Communities</br></br></h5>
                 <p><b>UN SDG 11</b></p>
-                <p>Make cities and human settlements inclusive, safe, resilient and sustainable</p>
+                <p>Paying special attention to municipal and other waste management</p>
               </li>
             </ul>
           </div>
