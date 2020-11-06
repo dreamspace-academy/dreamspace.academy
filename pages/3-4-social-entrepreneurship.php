910666ec81435e2507dd8ec9960c0b34b1678406
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-11-07 00:12:58 +0530

    Update dreamspace terrace

--- pages/3-4-social-entrepreneurship.php
+++ pages/3-4-social-entrepreneurship.php
@@ -1,5 +1,19 @@
 <?php include('0-0-header.php') ?>
 <div class="template-content">
+  <div class="template-component-header-subheader">
+    <h2>Social Enterprise</h2>
+    <p>We balance our social mission with business ventures through community business and socio economic development .</p>
+    <div></div>
+  </div>
+
+  <div class="template-component-divider template-component-divider-style-1"></div>
+
+  <div class="template-content-section template-content-layout template-content-layout-sidebar-right template-main template-clear-fix ">
+    <div class="template-content-layout-column-center">
+      <ul class="template-post-list">
 	<?php include('3-4-terrace.php') ?>
+</ul>
+</div>
+</div>
 </div>
 <?php include('9-0-footer.php') ?>
--- pages/3-4-terrace.php
+++ pages/3-4-terrace.php
@@ -7,19 +7,28 @@
 						<img src="../media/images/enterprise-terrace/terrace-dreamspace-1.jpg" data-thumb="../media/images/enterprise-terrace/terrace-dreamspace-1.jpg" alt="Terrace Community" />
 						<img src="../media/images/enterprise-terrace/terrace-dreamspace-2.jpg" data-thumb="../media/images/enterprise-terrace/terrace-dreamspace-2.jpg" alt="Terrace Community" />
 						<img src="../media/images/enterprise-terrace/terrace-dreamspace-3.jpg" data-thumb="../media/images/enterprise-terrace/terrace-dreamspace-3.jpg" alt="Terrace Community" />
+						<img src="../media/images/enterprise-terrace/terrace-dreamspace-4.jpg" data-thumb="../media/images/enterprise-terrace/terrace-dreamspace-4.jpg" alt="Terrace Community" />
 					</div>
 				</div>
 			</div>
 			<div class="template-layout-column-right template-margin-bottom-reset">
 				<div class="template-align-center">
-					<h2>Terrace</h2>
+					<h2>DreamSpace Terrace</h2>
 					<div class="template-component-divider template-component-divider-style-2"></div>
 					<div class="template-component-italic template-margin-top-3">
-						Terace is our outdoor activity center
+						Terrace is our outdoor activity center
 					</div>
 					<p class="template-margin-top-3">
 						It is a public location where members of a community tend to gather for group activities, social support, public information, and other purposes.
 					</p>
+				      </br>
+							    </br>
+					<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+						<ul>
+							<li><a target="_blank" href="" class="template-component-social-icon-linkedin"></a></li>
+							<li><a target="_blank" href="https://dreamspace-terrace.business.site/" class="template-component-social-icon-rss"></a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
