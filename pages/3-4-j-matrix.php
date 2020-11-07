9694b22e9d3361896f4b1c2a1353e5ccfd78c94d
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-11-07 21:57:13 +0530

    Add J Matrix to social enterprise

---
+++ pages/3-4-j-matrix.php
@@ -0,0 +1,35 @@
+<div class="template-content-section template-padding-bottom-5 template-background-color-2" id="j-matrix">
+	<div class="template-main">
+		<div class="template-layout-50x50 template-clear-fix">
+			<div class="template-layout-column-left template-margin-bottom-reset">
+				<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+					<div>
+						<img src="../media/images/enterprise-j-matrix/j-matrix-dreamspace-1.jpg" data-thumb="../media/images/enterprise-j-matrix/j-matrix-dreamspace-1.jpg" alt="j-matrix Community" />
+						<img src="../media/images/enterprise-j-matrix/j-matrix-dreamspace-2.jpg" data-thumb="../media/images/enterprise-j-matrix/j-matrix-dreamspace-2.jpg" alt="j-matrix Community" />
+						<img src="../media/images/enterprise-j-matrix/j-matrix-dreamspace-3.jpg" data-thumb="../media/images/enterprise-j-matrix/j-matrix-dreamspace-3.jpg" alt="j-matrix Community" />
+						<img src="../media/images/enterprise-j-matrix/j-matrix-dreamspace-4.jpg" data-thumb="../media/images/enterprise-j-matrix/j-matrix-dreamspace-4.jpg" alt="j-matrix Community" />
+					</div>
+				</div>
+			</div>
+			<div class="template-layout-column-right template-margin-bottom-reset">
+				<div class="template-align-center">
+					<h2>J Matrix</h2>
+					<div class="template-component-divider template-component-divider-style-2"></div>
+					<div class="template-component-italic template-margin-top-3">
+						J Matrix is a 3D modelling and designing enterprise
+					</div>
+					<p class="template-margin-top-3">
+						We develop different 3D models for social experments and interractions. We provide educational materials for person with desabilities and kids. 
+					</p>
+				      </br>
+							    </br>
+					<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+						<ul>
+							<li><a target="_blank" href="" class="template-component-social-icon-rss"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
