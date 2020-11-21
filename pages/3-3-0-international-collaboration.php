919b8b130cd0383396b5884907e4c58441626d78
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-11-21 12:50:07 +0530

    Fix bug in international colabration

--- pages/3-3-0-international-collaboration.php
+++ pages/3-3-0-international-collaboration.php
@@ -3,10 +3,16 @@
   <div class="template-component-header-subheader">
     <h2>International Collaboration</h2>
     <p>Our co-creation operation with international partners to expand our visibility and knowledge base .</p>
-    <div></div>
-    </div>
+  </div>
+    <div class="template-component-divider template-component-divider-style-1"></div>
+      <div class="template-content-section template-content-layout template-content-layout-sidebar-right template-main template-clear-fix ">
+          <div class="template-content-layout-column-center">
+                  <ul class="template-post-list">
         <?php include('3-3-story-telling-nico.php');  ?>
         <?php include('3-3-design-thinking-maite.php');  ?>
+              </ul>
+    </div>
+    </div>
     </div>
 
 <?php include('9-0-footer.php') ?>
