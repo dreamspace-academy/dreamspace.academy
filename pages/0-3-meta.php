f8663d7d92408cc61d4f27c0c20296c7a707089f
Aravinth Panch <me@aravinth.info>
2020-12-06 15:26:51 +0100

    Update meta title

--- pages/0-3-meta.php
+++ pages/0-3-meta.php
@@ -3,20 +3,20 @@
 <meta name="description" content="DreamSpace Academy is a community innovation centre to tackle complex local socio-economic and environmental challenges with challenge-based learning.">
 
 <!-- Google / Search Engine Tags -->
-<meta itemprop="name" content="DreamSpace Academy">
+<meta itemprop="name" content="DreamSpace Academy | Community Innovation Center | Sri Lanka">
 <meta itemprop="description" content="DreamSpace Academy is a community innovation centre to tackle complex local socio-economic and environmental challenges with challenge-based learning.">
 <meta itemprop="image" content="../media/images/org-life-cycle/dreamspace-academy.jpg">
 
 <!-- Facebook Meta Tags -->
 <meta property="og:url" content="https://dreamspace.academy">
 <meta property="og:type" content="website">
-<meta property="og:title" content="DreamSpace Academy">
+<meta property="og:title" content="DreamSpace Academy | Community Innovation Center | Sri Lanka">
 <meta property="og:description" content="DreamSpace Academy is a community innovation centre to tackle complex local socio-economic and environmental challenges with challenge-based learning.">
 <meta property="og:image" content="../media/images/org-life-cycle/dreamspace-academy.jpg">
 
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="DreamSpace Academy">
+<meta name="twitter:title" content="DreamSpace Academy | Community Innovation Center | Sri Lanka">
 <meta name="twitter:description" content="DreamSpace Academy is a community innovation centre to tackle complex local socio-economic and environmental challenges with challenge-based learning.">
 <meta name="twitter:image" content="../media/images/org-life-cycle/dreamspace-academy.jpg">
 
