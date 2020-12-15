f4943486152658e7c970e1082439225c0126514f
Aravinth Panch <me@aravinth.info>
2020-12-15 23:02:46 +0100

    Add awards to index page

---
+++ pages/1-awards.php
@@ -0,0 +1,93 @@
+<div class="template-content-section template-padding-top-reset template-background-color-0">
+
+	<div class="template-main">
+
+		<div class="template-component-header-subheader">
+			<h3>What are the awards under our belt?</h3>
+			<h6>Our achievements at international to national competitions & recognitions</h6>
+			<div></div>
+		</div>
+
+		<!-- Counter box -->
+		<div class="template-component-counter-box template-state-carousel-enable">
+
+			<!-- Layout 33x33x33 -->
+			<ul class="template-layout-33x33x33 template-clear-fix">
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-earth-company.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Earth Company - Impact Heroes</br>2021 - Indonesia</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-peace-direct.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Peace Direct - Peacebuilding</br>2020 - USA</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-global-community-bio-fellowship.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Global Community Bio Fellowship</br>2020 - USA</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-genu.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Generation Unlimited - Youth Challenge</br>2020 - Sri Lanka</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-sjwp.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Stockholm Junior Water Prize</br>2020 - Sweden</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-hackdev.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Hackdev - Social Innovation Challenge</br>2019 - Sri Lanka</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<!-- Counter -->
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/images/org-awards/awards-dreamspace-falling-walls.jpg" alt="dreamspace-academy" />
+					</div>
+
+					<!-- Header -->
+					<h6>Falling walls - Sciece Engagement</br>2019 - Germany</h6>
+					<a href="2-0-about.php#awards" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+			</ul>
+
+			<!-- Pagination -->
+			<div class="template-pagination template-pagination-style-1"></div>
+
+		</div>
+	</div>
+</div>
