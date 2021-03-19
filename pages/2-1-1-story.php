d284e6c2f7e927a8584e91524eab626bbb9f494f
Aravinth Panch <me@aravinth.info>
2021-03-19 16:21:57 +0100

    Add story section

---
+++ pages/2-1-1-story.php
@@ -0,0 +1,12 @@
+<div class="template-main" id="story">
+	<!-- Header and subheader -->
+	<div class="template-component-header-subheader">
+		<h2>Our Story</h2>
+		<h6></h6>
+		<div></div>
+	</div>
+
+	<div class="template-content-section template-padding-top-reset">
+		<embed src="https://files.aravinth.info/decks/dreamspace-academy-story-deck.pdf#page=1&view=fit&toolbar=0" type="application/pdf" height=500px width="100%" class="responsive">
+	</div>
+</div>
