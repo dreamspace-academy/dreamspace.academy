fc754c584bf3aef3e3d25302091b49adfe6f0501
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-04-20 19:30:40 +0530

    Add mentors & remove advisors page

--- pages/2-1-4-mentors.php
+++ pages/2-1-4-mentors.php
@@ -145,6 +145,101 @@
             </div>
           </div>
         </li>
+        <li class="template-layout-column-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media\images\org-advisors\supporters-dreamspace-stephen.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Stephen Byam</h6>
+              </br>
+              </br>
+              <span>Finance Consultant</span>
+              <span>USA</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/stephen-byam-38a1a91a/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media\images\org-advisors\supporters-dreamspace-peter.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Peter Glasser</h6>
+              </br>
+              </br>
+              <span>Finance Consultant</span>
+              <span>Switzerland</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.facebook.com/peter.glaser.353">
+                    <i class="fab fa-facebook fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media\images\org-advisors\supporters-dreamspace-gabriella.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Dr. Gabriella Dezsö</h6>
+              </br>
+              </br>
+              <span>Startup Business Mentor</span>
+              <span>Switzerland</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/gabrielladezso/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media\images\org-advisors\supporters-dreamspace-philipp.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Philipp Klink</h6>
+              </br>
+              </br>
+              <span>Aerospace Engineer</span>
+              <span>Australia</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/philipp-klink-198835b9/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media\images\org-advisors\supporters-dreamspace-noelene.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Noelene Palmer</h6>
+              </br>
+              </br>
+              <span>Business Development</span>
+              <span>Australia</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/noelenepalmer">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
