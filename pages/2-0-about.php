d2dfc35293a940494b17e88f3e2aace78320b85f
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-04-20 19:31:09 +0530

    Remove advisors from about page

--- pages/2-0-about.php
+++ pages/2-0-about.php
@@ -6,6 +6,5 @@
 <?php include('2-1-3-achievements.php') ?>
 <?php include('2-1-4-mentors.php') ?>
 <?php include('2-1-5-partners.php') ?>
-<?php include('2-1-7-advisors.php') ?>
 </div>
 <?php include('9-0-footer.php') ?>
