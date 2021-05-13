195e29cf00bc229dbafb8eb9c523160bb30f509e
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-05-13 22:57:56 +0530

    Add HER & AI4Life under peacebuilding

--- pages/4-1-0-peacebuilding.php
+++ pages/4-1-0-peacebuilding.php
@@ -1,7 +1,7 @@
 <?php include('0-0-header.php') ?>
 <div class="template-content">
   <div class="template-component-header-subheader">
-    <h2>Peacebuilding</h2>
+    <h2>Peacebuilding & Reconciliation</h2>
     <p></p>
     <div></div>
   </div>
@@ -9,7 +9,7 @@
   <!--<?php include('4-1-1-sylc.php'); ?>-->
   <?php include('4-1-2-ireact.php'); ?>
   <?php include('4-1-3-digi-advocates.php'); ?>
-  <!--<?php include('4-1-4-her.php'); ?>
-  <?php include('4-1-5-ai4life.php'); ?>-->
+  <?php include('4-1-4-her.php'); ?>
+  <?php include('4-1-5-ai4life.php'); ?>
 </div>
 <?php include('9-0-footer.php') ?>
---
+++ pages/4-1-4-her.php
@@ -0,0 +1,35 @@
+<div class="template-content-section template-padding-bottom-5" id="her">
+	<div class="template-main">
+		<div class="template-layout-50x50 template-clear-fix">
+
+			<div class="template-layout-column-left template-margin-bottom-reset">
+				<div class="template-align-center">
+					<h2>HER</h2>
+					<div class="template-component-divider template-component-divider-style-2"></div>
+					<div class="template-component-italic template-margin-top-3">
+						We stand with HER
+					</div>
+					<p class="template-margin-top-3">
+						It is a woman empowerment initiative. We empower women through education, skill-based learning, tech education and career guidance. Because we believe empowered women empower women.</p>
+					</br>
+					</br>
+					<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+						<a target="_blank" href="https://www.facebook.com/hersrilanka" class="template-component-button template-component-button-style-1">More<i></i></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="template-layout-column-right template-margin-bottom-reset">
+				<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+					<div>
+						<img src="../media/gallery/enterprise-her-dreamspace-1.jpg" data-thumb="../media/gallery/enterprise-her-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-her-dreamspace-2.jpg" data-thumb="../media/gallery/enterprise-her-dreamspace-2.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-her-dreamspace-3.jpg" data-thumb="../media/gallery/enterprise-her-dreamspace-3.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-her-dreamspace-4.jpg" data-thumb="../media/gallery/enterprise-her-dreamspace-4.jpg" alt="DreamSpace Academy" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
