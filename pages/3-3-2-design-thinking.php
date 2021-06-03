deb967c0e610fe4d38dc1ea164e7fbb2040c70df
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-06-04 00:47:47 +0530

    Update design thinking content

--- pages/3-3-2-design-thinking.php
+++ pages/3-3-2-design-thinking.php
@@ -35,20 +35,37 @@
     <div class="template-post-section-content">
       <div class="template-post-content">
         <b style="color: #56509F">Expertise</b>
-        <p>Maite’s core experience is in impact-driven entrepreneurship, human-centric design and agile product development. She has a strong passion for bridging the gap between humanity and technology. </p>
+        <p>Maite is partner at The Morph Company, a Berlin-based consultancy for Innovation and
+Transformation. Her core experience is in impact-driven tech entrepreneurship, Design Thinking and
+agile product development.</br>While working in Europe, Canada, Cuba, Maite has experienced innovation approaches in different
+regions of the world. Today she helps organisations, their leaders and teams to establish authentic
+relationships with their users and to innovate with purpose in our rapid changing digital age.</p>
 
         <b style="color: #56509F">Workshop</b>
-        <p>Design Thinking Workshops were attended by 11 participants including the core team of DreamSpace Academy. Maite provided all the necessary techniques and methodologies to apply design thinking in anything from business to personal life.</p>
+        <p>Maite gave a five-days Design Thinking training to 16 members of the core team of the DreamSpace
+Academy. In this workshop, the team worked on designing user-centric solution ideas to specific
+challenges related to the DreamSpace Academy. The training consisted of a combination of
+theoretical inputs and hands-on team work. The participants applied different methods and tools for
+qualitative user research, creative ideation, prototyping, testing and iteration of their solution ideas.</p>
 
         <b style="color: #56509F">Impact</b>
-        <p>One of the participants, Manickam Elankumaran, has been applying Design Thinking in his teaching career to help his students design their life.</p>
+        <p>After framing and redefining the initial problems, the teams specifically worked on the strategic
+challenges of “How to gain trust from the parents of young boys and girls in Batticaloa for innovative
+and non-conventional projects of DreamSpace”? and “How to create meaningful partnerships with
+schools and other relevant institutions in Batticaloa?”</br>Besides developing concrete ideas for the application in future projects of the DreamSpace
+Academy, this training also allowed the team to sharpen its ability to develop innovative ideas and
+learn how to pass this spirit on to other members of the organization. The team also reflected
+around the attitudes, values and principles associated with user-centered design.</p>
 
       </div>
     </div>
 
     <div class="template-component-blockquote">
       <p>
-        Wonderful team
+        The DreamSpace Academy has a brilliant approach to empower local youth, to activate and
+encourage them to solve the challenges of their communities. I believe that they are carrying out an
+extremely important mission for Eastern Sri Lanka.</br>It was very impressive to see the team’s dedication and how they dream and work for a better
+future!
       </p>
       <span>Maite Hernandez Zubeldia</span>
     </div>
