f27b7c65a44f3b4642365211fedca31a6bb3249f
Aravinth Panch <me@aravinth.info>
2021-06-09 19:12:56 +0200

    Add dreamfungi forwarder

--- files/dreamfungi/index.php
+++ files/dreamfungi/index.php
@@ -1,2 +1,26 @@
-<?php include('../../pages/0-1-cookies-consent.php');  ?>
-<embed src="dreamspace-academy-dreamfungi.pdf" type="application/pdf" height="100%" width="100%" class="responsive">
+<script>
+  setTimeout(redirect, 2000);
+  var redirected = false;
+
+  function redirect() {
+    if (!redirected) {
+      redirected = true;
+      location.href = 'https://opencollective.com/dreamfungi';
+    }
+  }
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-168585905-2', 'auto');
+  ga('send', 'event', 'opencollective', 'redirect', {
+    hitCallback: redirect
+  });
+</script>
