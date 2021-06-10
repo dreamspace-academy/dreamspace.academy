644b330d157a2ba2bd6ec56f1abe31f517a70a47
Aravinth Panch <me@aravinth.info>
2021-06-10 19:13:13 +0200

    Add statistics section

---
+++ pages/1-3-1-statistics.php
@@ -0,0 +1,78 @@
+<div class="template-content-section template-padding-top-reset">
+	<div class="template-main">
+		<div class="template-component-header-subheader">
+			<h3>What impact have we co-created?</h3>
+			<h6>Story from zero to hero</h6>
+			<div></div>
+		</div>
+			<div class="template-component-counter-box template-state-carousel-enable">
+				<ul class="template-layout-25x25x25x25 template-clear-fix">
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">528</span>
+							<span class="template-component-counter-box-counter-character">+</span>
+						</span>
+						<h5>Candidates</h5>
+						<p>empowered from underserved communities</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">14</span>
+						</span>
+						<h5>Labs</h5>
+						<p>built for interdisciplinary education and co-creation</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">9</span>
+							<span class="template-component-counter-box-counter-character">+</span>
+						</span>
+						<h5>Innovations</h5>
+						<p>developed to tackle complex local challenges</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">8</span>
+							<span class="template-component-counter-box-counter-character">+</span>
+						</span>
+						<h5>Ventures</h5>
+						<p>built by the empowered candidates</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">23</span>
+						</span>
+						<h5>Medals</h5>
+						<p>received from The Duke of Edinburgh</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+					<li class="template-layout-column-left">
+						<span class="template-component-counter-box-counter">
+							<span class="template-component-counter-box-counter-value">11</span>
+							<span class="template-component-counter-box-counter-character">+</span>
+						</span>
+						<h5>Achievements</h5>
+						<p>won at at international & national stages</p>
+						<span class="template-component-counter-box-timeline template-state-hidden">
+							<span></span>
+						</span>
+					</li>
+				</ul>
+				<div class="template-pagination template-pagination-style-1"></div>
+			</div>
+	</div>
+</div>
