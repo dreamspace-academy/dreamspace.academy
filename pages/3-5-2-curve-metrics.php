c10342de19c7544db5a0ebbaaea265a1ec6404b6
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-08-10 00:14:35 +0530

    Change Curve Metrics logo & name

---
+++ pages/3-5-2-curve-metrics.php
@@ -0,0 +1,35 @@
+<div class="template-content-section template-padding-bottom-5" id="curve-metrics">
+	<div class="template-main">
+		<div class="template-layout-50x50 template-clear-fix">
+
+			<div class="template-layout-column-left template-margin-bottom-reset">
+				<div class="template-align-center">
+					<h2>Curve Metrics</h2>
+					<div class="template-component-divider template-component-divider-style-2"></div>
+					<div class="template-component-italic template-margin-top-3">
+						Arts and craft shop
+					</div>
+					<p class="template-margin-top-3">
+						Curve Metrics is a craft marketing enterprise. It's core business is retail of all kind of craft using our e-commerce site. It focuses in empowering women and disable people in the community.</p>
+					</br>
+					</br>
+					<div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+						<a target="_blank" href="https://www.facebook.com/CurveMatrics" class="template-component-button template-component-button-style-1">More<i></i></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="template-layout-column-right template-margin-bottom-reset">
+				<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+					<div>
+						<img src="../media/gallery/enterprise-curve-metrics-dreamspace-1.jpg" data-thumb="../media/gallery/enterprise-curve-metrics-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-curve-metrics-dreamspace-2.jpg" data-thumb="../media/gallery/enterprise-curve-metrics-dreamspace-2.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-curve-metrics-dreamspace-3.jpg" data-thumb="../media/gallery/enterprise-curve-metrics-dreamspace-3.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/enterprise-curve-metrics-dreamspace-4.jpg" data-thumb="../media/gallery/enterprise-curve-metrics-dreamspace-4.jpg" alt="DreamSpace Academy" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
