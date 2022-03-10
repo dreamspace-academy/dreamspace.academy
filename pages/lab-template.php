b88e3dd39f4f8e0601c56b4f41854fe65cee9ed9
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-03-10 17:18:57 +0530

    create template for labs

---
+++ pages/lab-template.php
@@ -0,0 +1,165 @@
+<?php include('0-0-header.php') ?>
+<div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
+	<div class="template-main">
+		<div class="template-component-header-subheader">
+			<h3>Title</h3>
+			<h6>Sub Title</h6>
+			<div></div>
+		</div>
+		<div class="template-component-counter-box">
+			<ul class="template-layout-25x25x25x25 template-clear-fix">
+				<li class="template-layout-column-left">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-electronics-dreamspace-1.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Electronics Lab</h5>
+					<div>
+						<p>For those who are fascinated by robots, to develop practical skills for building anything from circuits to humanoid robotics</p>
+						</br>
+						</br>
+					</div>
+					<a href="3-1-0-maker-education.php#electronics-lab" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-left">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-software-dreamspace-1.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Software Lab</h5>
+					<div>
+						<p>For those who crave digital adrenaline, to gain practical experience in coding anything from apps to cloud computing</p>
+						</br>
+						</br>
+					</div>
+					<a href="3-1-0-maker-education.php#software-lab" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-right">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-mechanics-dreamspace-7.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Mechanics Lab</h5>
+					<div>
+						<p>For those who shift their brain gears to gather practical skills for anything from mechanical design to digital fabrication</p>
+						</br>
+						</br>
+					</div>
+					<a href="3-1-0-maker-education.php#mechanics-lab" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-right">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-business-dreamspace-1.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Business Lab</h5>
+					<div>
+						<p>For those with an entrepreneurial mindset, to acquire practical knowledge for maximising the growth of business ventures</p>
+						</br>
+					</div>
+					<a href="3-1-0-maker-education.php#business-lab" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+			</ul>
+			<div class="template-pagination template-pagination-style-1"></div>
+		</div>
+	</div>
+</div>
+
+<div class="template-content" id="team">
+
+  <!-- Header and subheader -->
+  <div class="template-component-header-subheader">
+    <h2>Title</h2>
+    <h6>Subtitle</h6>
+    <div></div>
+  </div>
+
+  <!-- Section -->
+  <div class="template-content-section template-padding-bottom-5 template-main">
+
+    <!-- Gallery -->
+    <div class="template-component-gallery">
+      <ul class="template-layout-25x25x25x25 template-clear-fix">
+        <li class="template-layout-column-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/people/kishoth-navaretnarajah-dreamspace.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Kishoth</br>Navaretnarajah</h6>
+              </br>
+              <span>Co-founder</br>Co-director</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/kishoth-navaretnarajah-342b4311a/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+                <li><a target="_blank" href="http://team.dreamspace.academy/kishoth/">
+                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-left">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/people/aravinth-panch-dreamspace.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Aravinth</br>Panch</h6>
+              </br>
+              <span>Co-founder</br>Co-director</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/aravinthpanch/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+                <li><a target="_blank" href="http://aravinth.info/">
+                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-center-right">
+        <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/people/anna-jeyaraj-dreamspace.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Anna</br>Jeyaraj Moses</h6>
+              </br>
+              <span>Co-director</span>
+              </br>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/anna-jeyaraj-moses-379258220">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+                <li><a target="_blank" href="https://www.facebook.com/Annie.Moses.J">
+                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="template-layout-column-right">
+          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+            <img src="../media/people/jayanthan-amalanathan-dreamspace.jpg" alt="DreamSpace Academy" />
+            <div>
+              <h6>Jayanthan</br>Amalanathan</h6>
+              </br>
+              <span>Guardian</br>Trash For Trade & EMD Labs</span>
+            </div>
+            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+              <ul>
+                <li><a target="_blank" href="https://www.linkedin.com/in/jayanthan-amalanathan-1020a4183/">
+                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+                <li><a target="_blank" href="http://team.dreamspace.academy/jayanthan/">
+                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                  </a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<?php include('9-0-footer.php') ?>
