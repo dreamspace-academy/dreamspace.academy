0deac5a76f174b62bab0f804eaeff819d0593bd3
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-06-14 20:54:04 +0530

    Update menubar

--- pages/0-3-menu-bar-responsive.php
+++ pages/0-3-menu-bar-responsive.php
@@ -61,7 +61,7 @@
 
 						<!-- Maker Education Menu -->
 						<li>
-							<a href="#">Maker Education<span></span></a>
+							<a href="#">Challenge Based Learning<span></span></a>
 							<ul>
 								<li><a href="3-1-0-maker-education.php#electronics-lab">Electromechanics Lab</a></li>
 								<li><a href="3-1-0-maker-education.php#software-lab">Software Lab</a></li>
