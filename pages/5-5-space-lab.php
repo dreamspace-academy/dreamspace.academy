07819f2de3b18844a209b2f7d9a86499e1ccd845
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-06-23 20:25:33 +0530

    Add space lab infrastructure

--- pages/5-5-space-lab.php
+++ pages/5-5-space-lab.php
@@ -22,7 +22,7 @@
 	</div>
 </div>
 
-<!-- <div class="template-main template-padding-bottom-5" id="infrastructure">
+<div class="template-main template-padding-bottom-5" id="infrastructure">
 	<div class="template-component-header-subheader">
 		<h2>Infrastructure</h2>
 		<h6></h6>
@@ -32,18 +32,15 @@
 	<div class="template-layout-column-left">
 		<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
 			<div>
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-1.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-1.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-2.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-2.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-3.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-3.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-4.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-4.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-5.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-5.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-6.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-6.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-7.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-7.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-space-lab-dreamspace-1.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-space-lab-dreamspace-2.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-space-lab-dreamspace-3.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-space-lab-dreamspace-4.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-4.jpg" alt="DreamSpace Academy" />
 			</div>
 		</div>
 	</div>
 
-</div> -->
+</div>
 
 <!-- <div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
 	<div class="template-main">
