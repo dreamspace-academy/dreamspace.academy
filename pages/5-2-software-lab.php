beeccce56aa786f986b81bb516331f801b9735b7
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-06-23 20:22:56 +0530

    Add software lab infrastructure

--- pages/5-2-software-lab.php
+++ pages/5-2-software-lab.php
@@ -23,29 +23,25 @@
 	</div>
 </div>
 
-<!-- <div class="template-main template-padding-bottom-5" id="infrastructure"> -->
-	<!-- Header and subheader --> 
-	<!-- <div class="template-component-header-subheader">
+<div class="template-main template-padding-bottom-5" id="infrastructure">
+	<div class="template-component-header-subheader">
 		<h2>Infrastructure</h2>
 		<h6></h6>
 		<div></div>
-	</div> -->
+	</div>
 
-	<!-- <div class="template-layout-column-left">
+	<div class="template-layout-column-left">
 		<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
 			<div>
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-1.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-1.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-2.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-2.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-3.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-3.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-4.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-4.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-5.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-5.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-6.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-6.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/bio-lab-infrastructure-dreamspace-7.jpg" data-thumb="../media/gallery/bio-lab-infrastructure-dreamspace-7.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-software-lab-dreamspace-1.jpg" data-thumb="../media/gallery/infrastructure-software-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-software-lab-dreamspace-2.jpg" data-thumb="../media/gallery/infrastructure-software-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-software-lab-dreamspace-3.jpg" data-thumb="../media/gallery/infrastructure-software-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-software-lab-dreamspace-4.jpg" data-thumb="../media/gallery/infrastructure-software-lab-dreamspace-4.jpg" alt="DreamSpace Academy" />
 			</div>
 		</div>
-	</div> -->
+	</div>
 
-<!-- </div> -->
+</div>
 
 <div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
 	<div class="template-main">
