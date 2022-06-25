3541873642f8bfb1a418f160784de4041cbf1f7e
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-06-25 22:13:22 +0530

    Update nano lab team

--- pages/5-8-nano-lab.php
+++ pages/5-8-nano-lab.php
@@ -1,42 +1,50 @@
 <?php include('0-0-header.php') ?>
 
-<div class="template-content-section template-padding-top-reset template-padding-bottom-reset"> 
-	<div class="template-main">
-		<div class="template-component-header-subheader">
-			<h3>
+<div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
+    <div class="template-main">
+        <div class="template-component-header-subheader">
+            <h3>
 
-				<span>
-					<div class="template-component-header-subheader">
-						<h2>Nano Lab</h2>
-						<h6></h6>
-						<div></div>
-					</div>
-					<p style="font-size:25px; color:#707070;">
-                    Nanotechnology is still a relatively novel area in Sri Lanka. We strongly believe that nanotechnology has the huge potential to transform people’s lives for the better. Our nano lab is established for creating opportunities for learning and discovering nanotechnology to ameliorate the future of Sri Lanka.Our mission is to give opportunities to the enthusiasts who have the finest mind to fiddle with molecule scale structures, and to build practical dexterity in nanotechnology.
+                <span>
+                    <div class="template-component-header-subheader">
+                        <h2>Nano Lab</h2>
+                        <h6></h6>
+                        <div></div>
+                    </div>
+                    <p style="font-size:25px; color:#707070;">
+                        Nanotechnology is still a relatively novel area in Sri Lanka. We strongly believe that
+                        nanotechnology has the huge potential to transform people’s lives for the better. Our nano lab
+                        is established for creating opportunities for learning and discovering nanotechnology to
+                        ameliorate the future of Sri Lanka.Our mission is to give opportunities to the enthusiasts who
+                        have the finest mind to fiddle with molecule scale structures, and to build practical dexterity
+                        in nanotechnology.
                     </p>
-				</span>
+                </span>
 
-			</h3>
-		</div>
-	</div>
+            </h3>
+        </div>
+    </div>
 </div>
 
 <div class="template-main template-padding-bottom-5" id="infrastructure">
-	<div class="template-component-header-subheader">
-		<h2>Infrastructure</h2>
-		<h6></h6>
-		<div></div>
-	</div>
+    <div class="template-component-header-subheader">
+        <h2>Infrastructure</h2>
+        <h6></h6>
+        <div></div>
+    </div>
 
-	<div class="template-layout-column-left">
-		<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
-			<div>
-				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
-			</div>
-		</div>
-	</div>
+    <div class="template-layout-column-left">
+        <div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+            <div>
+                <img src="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg"
+                    data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg"
+                    data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg"
+                    data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
+            </div>
+        </div>
+    </div>
 
 </div>
 
@@ -154,62 +162,116 @@
 	</div>
 </div> -->
 
-<!-- <div class="template-content" id="team">
+<div class="template-content" id="team">
 
-  <div class="template-component-header-subheader">
-    <h2>Team</h2>
-    <h6></h6>
-    <div></div>
-  </div>
-
-  <div class=" template-main">
-
-    <div class="template-component-gallery">
-      <ul class="template-layout-25x25x25x25 template-clear-fix">
-      <li class="template-layout-column-left">
-          <div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/people/aravinth-panch-dreamspace.jpg" alt="DreamSpace Academy" />
-            <div>
-              <h6>Aravinth</br>Panch</h6>
-              </br>
-              <span>Co-founder</br>Co-director</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/aravinthpanch/">
-                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
-                  </a></li>
-                <li><a target="_blank" href="http://aravinth.info/">
-                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
-                  </a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li class="template-layout-column-center-left">
-        	<div class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
-            <img src="../media/people/purusothman-murasolimaran-dreamspace.jpg" alt="DreamSpace Academy" />
-            <div>
-              <h6>Ravini</br>Hettiarachchi</h6>
-              </br>
-              <span>Guardian</br>Nano Lab</span>
-            </div>
-            <div class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
-              <ul>
-                <li><a target="_blank" href="https://www.linkedin.com/in/ravini-hettiarachchi-9456ab204">
-                    <i class="fab fa-linkedin-in fa-lg" style="color: white; padding-top: 17px;"></i>
-                  </a></li>
-                <li><a target="_blank" href="#">
-                    <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
-                  </a></li>
-              </ul>
-            </div>
-          </div>
-        </li>
-      </ul>
+    <div class="template-component-header-subheader">
+        <h2>Team</h2>
+        <h6></h6>
+        <div></div>
     </div>
-  </div>
-</div> -->
+
+    <div class=" template-main">
+
+        <div class="template-component-gallery">
+            <ul class="template-layout-25x25x25x25 template-clear-fix">
+                <li class="template-layout-column-left">
+                    <div
+                        class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+                        <img src="../media/people/aravinth-panch-dreamspace.jpg" alt="DreamSpace Academy" />
+                        <div>
+                            <h6>Aravinth</br>Panch</h6>
+                            </br>
+                            <span>Co-founder</br>Co-director</span>
+                        </div>
+                        <div
+                            class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+                            <ul>
+                                <li><a target="_blank" href="https://www.linkedin.com/in/aravinthpanch/">
+                                        <i class="fab fa-linkedin-in fa-lg"
+                                            style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                                <li><a target="_blank" href="http://aravinth.info/">
+                                        <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="template-layout-column-center-left">
+                    <div
+                        class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+                        <img src="../media/people/ravini-hettiarachi-dreamspace.jpg" alt="DreamSpace Academy" />
+                        <div>
+                            <h6>Ravini</br>Hettiarachchi</h6>
+                            </br>
+                            <span>Guardian</br>Nano Lab</span>
+                        </div>
+                        <div
+                            class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+                            <ul>
+                                <li><a target="_blank"
+                                        href="https://www.linkedin.com/in/ravini-hettiarachchi-9456ab204">
+                                        <i class="fab fa-linkedin-in fa-lg"
+                                            style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                                <li><a target="_blank" href="#">
+                                        <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="template-layout-column-center-right">
+                    <div
+                        class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+                        <img src="../media/people/vimosan-kanagarajah-dreamspace.jpg" alt="DreamSpace Academy" />
+                        <div>
+                            <h6>Vimosan</br>Kanagarajah</h6>
+                            </br>
+                            <span>Assistant</br>Nano Lab</span>
+                        </div>
+                        <div
+                            class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+                            <ul>
+                                <li><a target="_blank" href="https://www.linkedin.com/in/kanagarajah-vimosan-008826219">
+                                        <i class="fab fa-linkedin-in fa-lg"
+                                            style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                                <li><a target="_blank" href="https://www.facebook.com/kanagarajah.vimosan">
+                                        <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="template-layout-column-right">
+                    <div
+                        class="template-component-image template-component-image-hover-slide-enable template-fancybox template-preloader">
+                        <img src="../media/people/neelampary-balakirushnan-dreamspace.jpg" alt="DreamSpace Academy" />
+                        <div>
+                            <h6>Neelampary</br>Balakirushnan</h6>
+                            </br>
+                            <span>Assistant</br>Nano Lab</span>
+                        </div>
+                        <div
+                            class="template-component-social-icon template-component-social-icon-style-1 template-clear-fix">
+                            <ul>
+                                <li><a target="_blank"
+                                        href="https://www.linkedin.com/in/neelampary-balakirusnan-ba3808219">
+                                        <i class="fab fa-linkedin-in fa-lg"
+                                            style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                                <li><a target="_blank" href="#">
+                                        <i class="fas fa-link fa-lg" style="color: white; padding-top: 17px;"></i>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 <!-- <div class="template-content" id="mentors">
 
@@ -393,20 +455,23 @@
 </div> -->
 
 <div class="template-content-section template-padding-reset template-background-image">
-	<div class="template-main">
-		<div class="template-component-call-to-action template-component-call-to-action-style-2 template-section-white">
-			<div class="template-component-call-to-action-content">
-				<div class="template-component-call-to-action-content-left">
-					<h6>
-            If you want to be part of Peace Lab at DreamSpace Academy as an impact investor, partner, donor, team member or trainee, please reach out to us !!!
-          </h6>
-				</div>
-				<div class="template-component-call-to-action-content-right">
-					<a href="#" class="template-component-button template-component-button-style-2 template-section-white">Contact Us<i></i></a>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="template-main">
+        <div class="template-component-call-to-action template-component-call-to-action-style-2 template-section-white">
+            <div class="template-component-call-to-action-content">
+                <div class="template-component-call-to-action-content-left">
+                    <h6>
+                        If you want to be part of Peace Lab at DreamSpace Academy as an impact investor, partner, donor,
+                        team member or trainee, please reach out to us !!!
+                    </h6>
+                </div>
+                <div class="template-component-call-to-action-content-right">
+                    <a href="#"
+                        class="template-component-button template-component-button-style-2 template-section-white">Contact
+                        Us<i></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php include('9-0-footer.php') ?>
