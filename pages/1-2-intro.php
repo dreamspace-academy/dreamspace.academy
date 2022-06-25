173b23139d05816c7dfbb44ba5092442935dc40a
Aravinth Panch <me@aravinth.info>
2022-06-25 17:47:31 +0200

    refactor: format HTML code

--- pages/1-2-intro.php
+++ pages/1-2-intro.php
@@ -1,69 +1,77 @@
 <div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
-	<div class="template-main">
-		<div class="template-component-header-subheader">
-			<h3>
+    <div class="template-main">
+        <div class="template-component-header-subheader">
+            <h3>
 
-				<span>
-					<div class="template-component-header-subheader">
-						<h3>Why do we do what we do?</h3>
-						<h6></h6>
-						<div></div>
-					</div>
-					<p>
-						Three decades of <b style="color: #56509F">civil war</b> in Sri Lanka caused a massive impact on <b>socio-economy</b> and <b style="color: #56509F">environmental ecosystems</b>
-					</p>
-					<div class="template-margin-top-3"></div>
-					<p>
-						<b style="color: #56509F">International aid</b> provided significant support, but <b>imported solutions</b> couldn't solve fundamental <b style="color: #56509F">local problems sustainably</b>
-					</p>
-					<div class="template-margin-top-3"></div>
-					<p>
-						We set out to <b>empower locals</b> to solve </br>complex <b style="color: #56509F">local challenges !!!</b>
-					</p>
-				</span>
+                <span>
+                    <div class="template-component-header-subheader">
+                        <h3>Why do we do what we do?</h3>
+                        <h6></h6>
+                        <div></div>
+                    </div>
+                    <p>
+                        Three decades of <b style="color: #56509F">civil war</b> in Sri Lanka caused a massive impact on
+                        <b>socio-economy</b> and <b style="color: #56509F">environmental ecosystems</b>
+                    </p>
+                    <div class="template-margin-top-3"></div>
+                    <p>
+                        <b style="color: #56509F">International aid</b> provided significant support, but <b>imported
+                            solutions</b> couldn't solve fundamental <b style="color: #56509F">local problems
+                            sustainably</b>
+                    </p>
+                    <div class="template-margin-top-3"></div>
+                    <p>
+                        We set out to <b>empower locals</b> to solve </br>complex <b style="color: #56509F">local
+                            challenges !!!</b>
+                    </p>
+                </span>
 
-				<span>
-					<div class="template-component-header-subheader">
-						<h3>Who are we?</h3>
-						<h6></h6>
-						<div></div>
-					</div>
-					<p>
-						<b style="color: #56509F">DreamSpace Academy</b> is a <b style="color: #56509F">non-profit social enterprise</b> tackling local <b>socio-economic</b> and <b style="color: #56509F">environmental</b> challenges through <b>challenge-based
-							learning</b>,
-						<b style="color: #56509F">grassroots innovation</b> and <b>impact venture building</b>
-					</p>					
-				</span>
+                <span>
+                    <div class="template-component-header-subheader">
+                        <h3>Who are we?</h3>
+                        <h6></h6>
+                        <div></div>
+                    </div>
+                    <p>
+                        <b style="color: #56509F">DreamSpace Academy</b> is a <b style="color: #56509F">non-profit
+                            social enterprise</b> tackling local <b>socio-economic</b> and <b
+                            style="color: #56509F">environmental</b> challenges through <b>challenge-based
+                            learning</b>,
+                        <b style="color: #56509F">grassroots innovation</b> and <b>impact venture building</b>
+                    </p>
+                </span>
 
-				<span>
-					<div class="template-component-header-subheader">
-						<h3>What is our mission?</h3>
-						<h6></h6>
-						<div></div>
-					</div>
-					<p>
-						Our mission is to achieve <b style="color: #56509F">sustainable development goals</b> in Sri Lanka by empowering <b>underserved communities</b>, protecting <b style="color: #56509F">environmental ecosystems</b>, facilitating <b>peacebuilding</b>
-						and fostering <b style="color: #56509F">sustainable startups</b>
-					</p>
-					</br>
-					<center>
-						<img src="../media/others/svgs/social-mission-business-venture.svg" height="400" width="800" />
-					</center>
-					</br>
-				</span>
+                <span>
+                    <div class="template-component-header-subheader">
+                        <h3>What is our mission?</h3>
+                        <h6></h6>
+                        <div></div>
+                    </div>
+                    <p>
+                        Our mission is to achieve <b style="color: #56509F">sustainable development goals</b> in Sri
+                        Lanka by empowering <b>underserved communities</b>, protecting <b
+                            style="color: #56509F">environmental ecosystems</b>, facilitating <b>peacebuilding</b>
+                        and fostering <b style="color: #56509F">sustainable startups</b>
+                    </p>
+                    </br>
+                    <center>
+                        <img src="../media/others/svgs/social-mission-business-venture.svg" height="400" width="800" />
+                    </center>
+                    </br>
+                </span>
 
-				<span>
-					<div class="template-component-header-subheader">
-						<h3>How do we empower with DreamSpace Lifecycle?</h3>
-						<h6></h6>
-						<div></div>
-					</div>
-					<center>
-						<img src="../media/others/svgs/personalised-empowerment.svg" height="375" width="1000" />
-					</center>
-				</span>
+                <span>
+                    <div class="template-component-header-subheader">
+                        <h3>How do we empower with DreamSpace Lifecycle?</h3>
+                        <h6></h6>
+                        <div></div>
+                    </div>
+                    <center>
+                        <img src="../media/others/svgs/personalised-empowerment.svg" height="375" width="1000" />
+                    </center>
+                </span>
 
-			</h3>
-		</div>
-	</div>
+            </h3>
+        </div>
+    </div>
 </div>
--- pages/1-3-1-statistics.php
+++ pages/1-3-1-statistics.php
@@ -1,79 +1,79 @@
 <div class="template-content-section template-padding-top-reset">
-	<div class="template-main">
-		<div class="template-component-header-subheader">
-			<h3>Sustainability</h3>
-			<h6></h6>
-			<div></div>
-		</div>
-			<div class="template-component-counter-box template-state-carousel-enable">
-				<ul class="template-layout-25x25x25x25 template-clear-fix">
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">528</span>
-							<span class="template-component-counter-box-counter-character">+</span>
-						</span>
-						<h5>Candidates</h5>
-						<p>empowered from underserved communities</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">9</span>
-							<span class="template-component-counter-box-counter-character">+</span>
-						</span>
-						<h5>Labs</h5>
-						<p>being built for interdisciplinary education and co-creation</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">9</span>
-							<span class="template-component-counter-box-counter-character">+</span>
-						</span>
-						<h5>Innovations</h5>
-						<p>developed to tackle complex local challenges</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">8</span>
-							<span class="template-component-counter-box-counter-character">+</span>
-						</span>
-						<h5>Ventures</h5>
-						<p>built by the empowered candidates</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">23</span>
-						</span>
-						<h5>Medals</h5>
-						<p>received from The Duke of Edinburgh</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-					<li class="template-layout-column-left">
-						<span class="template-component-counter-box-counter counter-box-round">
-							<span class="template-component-counter-box-counter-value">11</span>
-							<span class="template-component-counter-box-counter-character">+</span>
-						</span>
-						<h5>Achievements</h5>
-						<p>won at at international & national stages</p>
-						<span class="template-component-counter-box-timeline template-state-hidden">
-							<span></span>
-						</span>
-					</li>
-				</ul>
-				<div class="template-pagination template-pagination-style-1"></div>
-			</div>
-	</div>
+    <div class="template-main">
+        <div class="template-component-header-subheader">
+            <h3>Sustainability</h3>
+            <h6></h6>
+            <div></div>
+        </div>
+        <div class="template-component-counter-box template-state-carousel-enable">
+            <ul class="template-layout-25x25x25x25 template-clear-fix">
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">528</span>
+                        <span class="template-component-counter-box-counter-character">+</span>
+                    </span>
+                    <h5>Candidates</h5>
+                    <p>empowered from underserved communities</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">9</span>
+                        <span class="template-component-counter-box-counter-character">+</span>
+                    </span>
+                    <h5>Labs</h5>
+                    <p>being built for interdisciplinary education and co-creation</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">9</span>
+                        <span class="template-component-counter-box-counter-character">+</span>
+                    </span>
+                    <h5>Innovations</h5>
+                    <p>developed to tackle complex local challenges</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">8</span>
+                        <span class="template-component-counter-box-counter-character">+</span>
+                    </span>
+                    <h5>Ventures</h5>
+                    <p>built by the empowered candidates</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">23</span>
+                    </span>
+                    <h5>Medals</h5>
+                    <p>received from The Duke of Edinburgh</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+                <li class="template-layout-column-left">
+                    <span class="template-component-counter-box-counter counter-box-round">
+                        <span class="template-component-counter-box-counter-value">11</span>
+                        <span class="template-component-counter-box-counter-character">+</span>
+                    </span>
+                    <h5>Achievements</h5>
+                    <p>won at at international & national stages</p>
+                    <span class="template-component-counter-box-timeline template-state-hidden">
+                        <span></span>
+                    </span>
+                </li>
+            </ul>
+            <div class="template-pagination template-pagination-style-1"></div>
+        </div>
+    </div>
 </div>
