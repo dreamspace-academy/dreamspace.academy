88e7b579e465d2228dec15ef5064b3e72952a5c7
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-06-25 21:57:12 +0530

    Add nano lab infrastructure

--- pages/5-8-nano-lab.php
+++ pages/5-8-nano-lab.php
@@ -21,7 +21,7 @@
 	</div>
 </div>
 
-<!-- <div class="template-main template-padding-bottom-5" id="infrastructure">
+<div class="template-main template-padding-bottom-5" id="infrastructure">
 	<div class="template-component-header-subheader">
 		<h2>Infrastructure</h2>
 		<h6></h6>
@@ -31,15 +31,14 @@
 	<div class="template-layout-column-left">
 		<div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
 			<div>
-				<img src="../media/gallery/infrastructure-space-lab-dreamspace-1.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/infrastructure-space-lab-dreamspace-2.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/infrastructure-space-lab-dreamspace-3.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
-				<img src="../media/gallery/infrastructure-space-lab-dreamspace-4.jpg" data-thumb="../media/gallery/infrastructure-space-lab-dreamspace-4.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-1.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-2.jpg" alt="DreamSpace Academy" />
+				<img src="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg" data-thumb="../media/gallery/infrastructure-nano-lab-dreamspace-3.jpg" alt="DreamSpace Academy" />
 			</div>
 		</div>
 	</div>
 
-</div> -->
+</div>
 
 <!-- <div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
 	<div class="template-main">
@@ -155,7 +154,7 @@
 	</div>
 </div> -->
 
-<div class="template-content" id="team">
+<!-- <div class="template-content" id="team">
 
   <div class="template-component-header-subheader">
     <h2>Team</h2>
@@ -210,7 +209,7 @@
       </ul>
     </div>
   </div>
-</div>
+</div> -->
 
 <!-- <div class="template-content" id="mentors">
 
