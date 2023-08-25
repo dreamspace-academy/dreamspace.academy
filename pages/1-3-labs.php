cb0b69fb7d9d4fc9b7d4e48a81a1286055c76790
Rishothman <rishanrishothman@gmail.com>
2023-08-25 18:24:33 +0530

    fix: add new labs

--- pages/1-3-labs.php
+++ pages/1-3-labs.php
@@ -9,9 +9,9 @@
 			<ul class="template-layout-25x25x25x25 template-clear-fix">
 				<li class="template-layout-column-left">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-electronics-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/lab-electronics-dreamspace-0.jpg" alt="DreamSpace Academy" />
 					</div>
-					<h5>Electromechanics Lab</h5>
+					<h5>Electronics Lab</h5>
 					<div>
 						<p>For those who are fascinated by robots, to develop practical skills for building anything from circuits to humanoid robotics</p>
 						</br>
@@ -21,7 +21,19 @@
 				</li>
 				<li class="template-layout-column-center-left">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-software-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/lab-mechanics-dreamspace-1.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Mechanics Lab</h5>
+					<div>
+						<p>For those who are fascinated by robots, to develop practical skills for building anything from circuits to humanoid robotics</p>
+						</br>
+						</br>
+					</div>
+					<a href="5-1-electromechanics-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-right">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-software-dreamspace-0.jpg" alt="DreamSpace Academy" />
 					</div>
 					<h5>Software Lab</h5>
 					<div>
@@ -31,29 +43,18 @@
 					</div>
 					<a href="5-2-software-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li>
-				<li class="template-layout-column-center-right">
+				<li class="template-layout-column-right">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-design-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/lab-design-dreamspace-0.jpg" alt="DreamSpace Academy" />
 					</div>
-					<h5>Story Lab</h5>
+					<h5>Media Lab</h5>
 					<div>
 						<p>For those who are fascinated by immersive narratives, to gain storytelling skillsets for communicating complex details to the common audience</p>
 						</br>
 					</div>
 					<a href="5-3-story-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li>
-				<li class="template-layout-column-right">
-					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-bio-dreamspace-0.jpg" alt="DreamSpace Academy" />
-					</div>
-					<h5>Bio Lab</h5>
-					<div>
-						<p>For those who want to get their hands wet with BioHacking, to build practical dexterity on BioTechnology</p>
-						</br>
-						</br>
-					</div>
-					<a href="5-4-bio-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
-				</li>
+				
 				<!-- <li class="template-layout-column-left">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
 						<img src="../media/gallery/lab-space-dreamspace-2.jpg" alt="DreamSpace Academy" />
@@ -65,9 +66,9 @@
 					</div>
 					<a href="5-5-space-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li> -->
-				<li class="template-layout-column-center-left">
+				<li class="template-layout-column-left">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-music-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/lab-music-dreamspace-0.jpg" alt="DreamSpace Academy" />
 					</div>
 					<h5>Music Lab</h5>
 					<div>
@@ -75,6 +76,18 @@
 						</br>
 					</div>
 					<a href="5-6-music-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-left">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-bio-dreamspace-0.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Bio Lab</h5>
+					<div>
+						<p>For those who want to get their hands wet with BioHacking, to build practical dexterity on BioTechnology</p>
+						</br>
+						</br>
+					</div>
+					<a href="5-4-bio-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li>
 				<!-- <li class="template-layout-column-center-right">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
@@ -98,9 +111,9 @@
 					</div>
 					<a href="5-8-nano-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li> -->
-				<li class="template-layout-column-left">
+				<li class="template-layout-column-center-right">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
-						<img src="../media/gallery/lab-business-dreamspace-1.jpg" alt="DreamSpace Academy" />
+						<img src="../media/gallery/lab-business-dreamspace-0.jpg" alt="DreamSpace Academy" />
 					</div>
 					<h5>Business Lab</h5>
 					<div>
@@ -120,11 +133,50 @@
 					</div>
 					<a href="5-10-ocean-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
 				</li>	 -->
-				<li class="template-layout-column-center-right">
+				<li class="template-layout-column-right">
 					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
 						<img src="../media/gallery/lab-regeneration-dreamspace.jpg" alt="DreamSpace Academy" />
 					</div>
 					<h5>Regeneration Lab</h5>
+					<div>
+						<p>
+						For those who are interested to tackle plastic waste management problem in Sri Lanka through an upcycling process.
+						</p>
+						</br>
+					</div>
+					<a href="5-11-regeneration-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-left">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-blue-dreamspace-0.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Blue Lab</h5>
+					<div>
+						<p>
+						For those who are interested to tackle plastic waste management problem in Sri Lanka through an upcycling process.
+						</p>
+						</br>
+					</div>
+					<a href="5-11-regeneration-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-left">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-leaders-dreamspace-0.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Leaders Lab</h5>
+					<div>
+						<p>
+						For those who are interested to tackle plastic waste management problem in Sri Lanka through an upcycling process.
+						</p>
+						</br>
+					</div>
+					<a href="5-11-regeneration-lab.php" class="template-component-button template-component-button-style-1">More<i></i></a>
+				</li>
+				<li class="template-layout-column-center-right">
+					<div class="template-component-image template-component-counter-box-counter template-component-image-hover-slide-enable template-fancybox template-preloader">
+						<img src="../media/gallery/lab-linguistic-dreamspace-0.jpg" alt="DreamSpace Academy" />
+					</div>
+					<h5>Linguistic Lab</h5>
 					<div>
 						<p>
 						For those who are interested to tackle plastic waste management problem in Sri Lanka through an upcycling process.
