b18a402898ee837954cca84a0a22a78b6b18709d
Rishothman <rishanrishothman@gmail.com>
2023-08-28 17:20:27 +0530

    feat: add new workshop page

--- pages/workshop.php
+++ pages/workshop.php
@@ -1,19 +1,26 @@
 <?php include('0-0-header.php'); ?>
 
-<div class="template-content">
-  <div class="template-main">
+<div class="template-main" id="story">
+    <!-- Header and subheader -->
+    <div class="template-component-header-subheader">
+        <h2>Our Workshop</h2>
+        <h6>
+            <p style="font-size:25px; color:#707070;">
+            The program commences with interdisciplinary workshops, identifying passionate individuals who aspire to create positive change. These changemakers come from diverse backgrounds and share a common drive to address real-world challenges.
+            </p>
+            <a
+            target="_blank" href="https://youtu.be/ffG-e6RL_tw?si=MzUvZ-Ay9OlmN4QT">https://www.youtube.com/changemaker-workshop</a>
+        </h6>
+        <div></div>
+    </div>
 
-		<div class="template-component-header-subheader">
-			<h3>Would you like to be a part of ExoLab mission - 9?</h3>
-			<h6>
-				Register via <a target="_blank" href="https://forms.gle/A1aZhWp64NQQvSGo9">ExoLab Mission - 9</a></br>
-			</h6>
-			<div></div>
-		</div>
-		<center>
-			<img src="../media/others/svgs/exolab-mission.svg" height="375" width="1000" />
-		</center>
-	</div>
+    <div class="template-content-section template-padding-top-reset">
+        <!-- <iframe src="https://www.youtube.com/watch?v=ffG-e6RL_tw" type="application/pdf" height=500px width="100%" class="responsive"></iframe> -->
+        <iframe height=500px width="100%" class="responsive"
+            src="https://www.youtube.com/embed/ffG-e6RL_tw?si=MkhMus2YLT2zIEw8" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
 </div>
-
 <?php include('9-0-footer.php'); ?>
