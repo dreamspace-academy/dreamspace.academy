ab53137c8ad21708297033293ce991b55a0211b0
Lojana96 <lojanajegatheeswaran@gmail.com>
2024-11-13 09:32:57 +0530

    feat:update infrastructure

--- pages/2-1-6-infrastucture.php
+++ pages/2-1-6-infrastucture.php
@@ -10,22 +10,22 @@
     <div class="template-layout-column-left">
         <div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
             <div>
-                <img src="../media/gallery/infrastructure-techo-park-dreamspace-1.jpg"
-                    data-thumb="../media/gallery/infrastructure-techo-park-dreamspace-1.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techo-park-dreamspace-2.jpg"
-                    data-thumb="../media/gallery/infrastructure-techo-park-dreamspace-2.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspce-3.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspce-3.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspace-4.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspace-4.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspace-5.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspace-5.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspace-6.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspace-6.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspace-7.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspace-7.jpg" alt="DreamSpace Academy" />
-                <img src="../media/gallery/infrastructure-techno-park-dreamspace-8.jpg"
-                    data-thumb="../media/gallery/infrastructure-techno-park-dreamspace-8.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-1.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-1.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-2.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-2.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-3.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-3.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-4.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-4.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-5.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-5.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-6.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-6.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-7.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-7.jpg" alt="DreamSpace Academy" />
+                <img src="../media/gallery/infrastructure-technopark-dreamspace-8.jpg"
+                    data-thumb="../media/gallery/infrastructure-technopark-dreamspace-8.jpg" alt="DreamSpace Academy" />
             </div>
         </div>
     </div>
