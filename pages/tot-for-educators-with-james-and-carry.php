782f3ff4729abb223c581bb4eb19fa63d1e93563
Rathees Koneswaran <ratheesreo@gmail.com>
2025-03-12 13:06:01 +0530

    feat: add new page for james and carry training

---
+++ pages/tot-for-educators-with-james-and-carry.php
@@ -0,0 +1,123 @@
+<?php include('0-0-header.php') ?>
+<div class="template-content">
+    <div class="template-component-header-subheader">
+        <h2>Teaching Technology and Design</h2>
+        <p>Workshop on "Integrating Technology and Critical Thinking into Teaching Protocols"</p>
+
+    </div>
+    <div class=" template-content-layout template-content-layout-sidebar-right template-main template-clear-fix ">
+        <div class="template-content-layout-column-center">
+            <!-- Header -->
+            <div class="template-post-section-header">
+
+                20 Jan - 24 Jan 2020
+            </div>
+            <ul class="template-post-list">
+                <!-- Post -->
+                <li id="tot-for-teacher">
+
+                    <!-- Post container -->
+                    <div class="template-post">
+
+                        <div class="template-main template-padding-bottom-5" id="gallery">
+
+                            <div class="template-layout-column-left">
+                                <div
+                                    class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+                                    <div>
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-1.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-1.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-2.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-2.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-3.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-3.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-4.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-4.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-5.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-5.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-6.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-6.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-7.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-7.jpg"
+                                            alt="DreamSpace Academy" />
+                                        <img src="../media/gallery/infrastructure-electronics-dreamspace-8.jpg"
+                                            data-thumb="../media/gallery/infrastructure-electronics-dreamspace-8.jpg"
+                                            alt="DreamSpace Academy" />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                        <!-- Excerpt -->
+                        <div class="template-post-section-content">
+                            <div class="template-post-content">
+                                <b style="color: #56509F">Organizers and Trainers</b>
+                                <p>Dr Nico Meißner is Senior Lecturer and Deputy Director (Learning & Teaching) at
+                                    Griffith Film School, Australia’s largest film school. He is researching and writing
+                                    on entrepreneurship in the creative industries and the intricate relations between
+                                    storytelling and innovation. He was the inaugural Dean of the Faculty of Cinematic
+                                    Arts in Malaysia and has taught documentary filmmaking, storytelling and
+                                    entrepreneurship in the UK, Finland, Portugal, Malaysia, Myanmar, Sri Lanka, Ghana,
+                                    Colombia and Australia.</p>
+
+                                <b style="color: #56509F">Workshop Info</b>
+                                <p>The workshop focused on the integration of Artificial Intelligence (AI) and Critical
+                                    Thinking into Teaching Protocols, with the aim of fostering progressive education
+                                    and student-centered classrooms. Participants were introduced to a variety of
+                                    innovative teaching protocols, including the Reflection Prompts, Gallery Walk
+                                    Feedback, Menti Meter Crowdsourcing, and several other essential protocols. These
+                                    protocols were designed to enhance student motivation, empower students to take an
+                                    active role in their learning, create memorable educational experiences, and enable
+                                    intentional teaching and curriculum design. During the workshop, the participant not
+                                    only learned these teaching protocols but also showcased their practical application
+                                    and proposed ways to incorporate these protocols into their existing curriculum and
+                                    classroom settings.</p>
+
+                                <b style="color: #56509F">Impact</b>
+                                <p>During the StoryLab workshop, we worked with 20 young people from Batticaloa,
+                                    building techniques and the confidence to tell authentic stories. The StoryMapping
+                                    workshop focused specifically on DreamSpace Academy and worked with seven core team
+                                    members. We tackled the problem of DreamSpace's visibility in the local community
+                                    and arrived at the Single Question: What if...we train youth ambassadors by solving
+                                    real-world problems in their communities and captured their journey through
+                                    authentic videos? We then spent the final day of our workshop to gather photographs
+                                    and turned them into an authentic Innovation Story.</p>
+
+                            </div>
+                        </div>
+
+                        <div class="template-component-blockquote">
+                            <p>DreamSpace Academy is doing important and inspiring work. I met many passionate,
+                                dedicated, forward-thinking and resourceful young people that deeply care about their
+                                community and innovating society in a human-centered, grassroots way.</p>
+                            <span>Dr. Nico Meissner</span>
+                        </div>
+
+                        <!-- "Continue reading" button -->
+                        <div class="template-post-section-button">
+                            <a href="3-1-0-maker-education.php#art-lab"
+                                class="template-component-button template-component-button-style-1">
+                                Register for Next Training<i></i>
+                            </a>
+                            <!-- Divider -->
+                            <div class="template-component-divider template-component-divider-style-1"></div>
+                        </div>
+
+                    </div>
+
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php include('9-0-footer.php') ?>
