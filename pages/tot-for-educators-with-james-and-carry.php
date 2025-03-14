4b9f5025702096de36c37224c25aeb7b231b8a70
Rathees Koneswaran <ratheesreo@gmail.com>
2025-03-14 09:02:09 +0530

    refactor: update file names

--- pages/tot-for-educators-with-james-and-carry.php
+++ pages/tot-for-educators-with-james-and-carry.php
@@ -1,77 +1,93 @@
 <?php include('0-0-header.php'); ?>
 
 <div class="template-main" id="story">
-     <div class="template-component-header-subheader">
-          <h2>Teaching Technology and Design</h2>
-          <p>Workshop on "Integrating Technology and Critical Thinking into Teaching Protocols"
-          </p>
-     </div>
+    <div class="template-component-header-subheader">
+        <h2>Teaching Technology and Design</h2>
+        <p>Workshop on "Integrating Technology and Critical Thinking into Teaching Protocols"
+        </p>
+    </div>
 
-     <div class="template-layout-column-left">
-          <p class="date">20 Jan - 24 Jan 2020</p>
-          <div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
-               <div>
-                    <img src="../media/gallery/Teacher-Training-workshop (1).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (1).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (2).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (2).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (3).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (3).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (4).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (4).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (5).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (5).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (6).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (6).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (7).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (7).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (8).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (8).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (9).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (9).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (10).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (10).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (11).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (11).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (12).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (12).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (13).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (13).png" alt="DreamSpace Academy" />
-                    <img src="../media/gallery/Teacher-Training-workshop (14).png"
-                         data-thumb="../media/gallery/Teacher-Training-workshop (1).png" alt="DreamSpace Academy" />
-               </div>
-          </div>
-     </div>
-     <div class="template-post">
-     <!-- content Container -->
-     <div class="template-post-section-content">
-          <div class="template-post-content">
-               <b style="color: #56509F">Organizers and Trainers</b>
-               <p>DreamSpace Academy Batticaloa organized the Teaching Technology And Design workshop with Carrie Leung and Benjamin James Simpson, who are Trainers in STEAMHEAD</p>
+    <div class="template-layout-column-left">
+        <p class="date">10 Apr - 14 Apr 2023</p>
+        <div class="template-component-nivo-slider template-component-nivo-slider-style-1 template-preloader">
+            <div>
+                <img src="../media/gallery/tot-james-and-carry-1.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-1.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-2.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-2.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-3.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-3.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-4.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-4.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-5.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-5.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-6.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-6.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-7.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-7.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-8.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-8.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-9.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-9.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-10.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-10.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-11.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-11.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-12.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-12.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-13.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-13.png" alt="DreamSpace Academy" />
+                <img src="../media/gallery/tot-james-and-carry-14.png"
+                    data-thumb="../media/gallery/tot-james-and-carry-1.png" alt="DreamSpace Academy" />
+            </div>
+        </div>
+    </div>
+    <div class="template-post">
+        <!-- content Container -->
+        <div class="template-post-section-content">
+            <div class="template-post-content">
+                <b style="color: #56509F">Organizers and Trainers</b>
+                <p>DreamSpace Academy organized the Teaching Technology and Design workshop with Carrie Leung and
+                    Benjamin James Simpson from STEAMHEAD from April 10th to 14th, 2023. </p>
 
-               <b style="color: #56509F">Workshop Info</b>
-               <p>The workshop focused on the integration of Artificial Intelligence (Al) and Critical Thinking into Teaching Protocols, with the aim of fostering progressive education and student-centered classrooms. Participants were introduced to a variety of innovative teaching protocols, including the Reflection Prompts, Gallery Walk Feedback, Menti Meter Crowdsourcing, and several other essential protocols. These protocols were designed to enhance student motivation, empower students to take an active role in their learning, create memorable educational experiences, and enable intentional teaching and curriculum design During the workshop, the participant not only learned these teaching protocols but also showcased their practical application and proposed ways to incorporate these protocols into their existing curriculum and classroom settings.
-               </p>
+                <b style="color: #56509F">Workshop Info</b>
+                <p>The workshop focused on the integration of Artificial Intelligence (Al) and Critical Thinking into
+                    Teaching Protocols, with the aim of fostering progressive education and student-centered classrooms.
+                    Participants were introduced to a variety of innovative teaching protocols, including the Reflection
+                    Prompts, Gallery Walk Feedback, Menti Meter Crowdsourcing, and several other essential protocols.
+                    These protocols were designed to enhance student motivation, empower students to take an active role
+                    in their learning, create memorable educational experiences, and enable intentional teaching and
+                    curriculum design During the workshop, the participant not only learned these teaching protocols but
+                    also showcased their practical application and proposed ways to incorporate these protocols into
+                    their existing curriculum and classroom settings.
+                </p>
 
-               <b style="color: #56509F">Impact</b>
-               <p>The workshop was held on April 10th, 2023. Our workshops have created a significant ripple effect in the local education community. During the training, teachers from Batticaloa schools and trainers from DreamSpace Academy received specialized instruction. Following the workshop, DreamSpace Academy trainers began leading interactive workshops for students and conducting Training of Trainers (TOT) sessions for school teachers.
-               <br><br>
-               This initiative has successfully reached more than 20 schools, fostering a collaborative learning environment and enhancing teaching practices. Due to the positive outcomes, teachers, schools, and the zonal education department have requested us to continue delivering TOT sessions across more institutions. Our trainers are actively providing ongoing workshops for teachers
+                <b style="color: #56509F">Impact</b>
+                <p>Our workshops have created a significant ripple effect in the local education community. During the
+                    training, teachers from Batticaloa schools and trainers from DreamSpace Academy received specialized
+                    instruction. Following the workshop, DreamSpace Academy trainers began leading interactive workshops
+                    for students and conducting Training of Trainers (TOT) sessions for school teachers.
+                    <br><br>
+                    This initiative has successfully reached more than 20 schools, fostering a collaborative learning
+                    environment and enhancing teaching practices. Due to the positive outcomes, teachers, schools, and
+                    the zonal education department have requested us to continue delivering TOT sessions across more
+                    institutions. Our trainers are actively providing ongoing workshops for teachers
 
-               </p>
+                </p>
 
-          </div>
-     </div>
+            </div>
+        </div>
 
-     <br><br>
-     <!-- Application -->
-     <div class="template-post-section-button">
-          <a href="https://forms.gle/C8PYWwPP3KR1FfwS7" class="template-component-button template-component-button-style-1">
-               REGISTER FOR NEXT TRAINING<i></i>
-          </a>
-          <!-- Divider -->
-          <div class="template-component-divider template-component-divider-style-1"></div>
-     </div>
-     </div>
+        <br><br>
+        <!-- Application -->
+        <div class="template-post-section-button">
+            <a href="https://forms.gle/C8PYWwPP3KR1FfwS7"
+                class="template-component-button template-component-button-style-1">
+                REGISTER FOR NEXT TRAINING<i></i>
+            </a>
+            <!-- Divider -->
+            <div class="template-component-divider template-component-divider-style-1"></div>
+        </div>
+    </div>
 </div>
 <?php include('9-0-footer.php'); ?>
