6880dc4ae57c730fbc868065f8b61c811ddd3de3
Govarthenan Rajadurai <govarthenan@gmail.com>
2026-04-09 11:46:00 +0530

    feat: add VisAct app privacy policy page

---
+++ pages/visact-privacy-policy.php
@@ -0,0 +1,99 @@
+<?php include('0-0-header.php'); ?>
+
+<div class="template-content">
+
+	<div class="template-content-section template-main">
+
+	<div class="template-component-header-subheader">
+		<h2>Privacy Policy</h2>
+		<h6>VISACT APP</h6>
+		<div></div>
+	</div>
+
+	<div class="template-post-section-content template-padding-top-reset">
+
+		<p>This privacy policy applies to the VisAct app (hereby referred to as "Application") for mobile devices that was created by <b>DreamSpace Academy</b> (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".</p>
+
+		<h5>Information Collection and Use</h5>
+
+		<p>The Application collects information when you download and use it. This information may include information such as:</p>
+		<ul style="margin-left: 20px; list-style-type: disc;">
+			<li>Your device's Internet Protocol address (e.g. IP address)</li>
+			<li>The pages of the Application that you visit, the time and date of your visit, the time spent on those pages</li>
+			<li>The time spent on the Application</li>
+			<li>The operating system you use on your mobile device</li>
+		</ul>
+
+		<p>The Application collects your device's location, which helps the Service Provider determine your approximate geographical location and make use of in below ways:</p>
+		<ul style="margin-left: 20px; list-style-type: disc;">
+			<li><b>Geolocation Services:</b> The Service Provider utilizes location data to provide features such as personalized content, relevant recommendations, and location-based services.</li>
+			<li><b>Analytics and Improvements:</b> Aggregated and anonymized location data helps the Service Provider to analyze user behavior, identify trends, and improve the overall performance and functionality of the Application.</li>
+			<li><b>Third-Party Services:</b> Periodically, the Service Provider may transmit anonymized location data to external services. These services assist them in enhancing the Application and optimizing their offerings.</li>
+		</ul>
+
+		<p>The Application does not use Artificial Intelligence (AI) technologies to process your data or provide features.</p>
+
+		<p>The Service Provider may use the information you provided to contact you from time to time to provide you with important information, required notices and marketing promotions.</p>
+
+		<p>For a better experience, while using the Application, the Service Provider may require you to provide us with certain personally identifiable information, including but not limited to email, username, age, gender, telephone. The information that the Service Provider request will be retained by them and used as described in this privacy policy.</p>
+
+		<h5>Permissions Requested</h5>
+
+		<p>The Application requests the following permissions to provide its services:</p>
+		<ul style="margin-left: 20px; list-style-type: disc;">
+			<li><b>Camera:</b> For capturing photos and videos to submit reports</li>
+			<li><b>Location (Fine & Coarse):</b> To determine your location for report submissions and personalized content</li>
+			<li><b>Microphone:</b> For audio recording in reports</li>
+			<li><b>Storage (Images & Audio):</b> To attach media files to reports</li>
+			<li><b>Internet:</b> For network connectivity and API communication</li>
+		</ul>
+
+		<h5>Third Party Access</h5>
+
+		<p>Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Service Provider may share your information with third parties in the ways that are described in this privacy statement.</p>
+
+		<p>The Service Provider may disclose User Provided and Automatically Collected Information:</p>
+		<ul style="margin-left: 20px; list-style-type: disc;">
+			<li>as required by law, such as to comply with a subpoena, or similar legal process;</li>
+			<li>when they believe in good faith that disclosure is necessary to protect their rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;</li>
+			<li>with their trusted services providers who work on their behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.</li>
+		</ul>
+
+		<h5>Opt-Out Rights</h5>
+
+		<p>You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.</p>
+
+		<h5>Data Retention Policy</h5>
+
+		<p>The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at <span class="brand_color_purple">dev@dreamspace.academy</span> and they will respond in a reasonable time.</p>
+
+		<h5>Children</h5>
+
+		<p>The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13.</p>
+
+		<p>The Service Provider does not knowingly collect personally identifiable information from children. The Service Provider encourages all children to never submit any personally identifiable information through the Application and/or Services. The Service Provider encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission. If you have reason to believe that a child has provided personally identifiable information to the Service Provider through the Application and/or Services, please contact the Service Provider <span class="brand_color_purple">(dev@dreamspace.academy)</span> so that they will be able to take the necessary actions. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf).</p>
+
+		<h5>Security</h5>
+
+		<p>The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect information the Service Provider processes and maintains.</p>
+
+		<h5>Changes</h5>
+
+		<p>This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to the Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.</p>
+
+		<p>This privacy policy is effective as of <b>2026-04-09</b></p>
+
+		<h5>Your Consent</h5>
+
+		<p>By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.</p>
+
+		<h5>Contact Us</h5>
+
+		<p>If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at <span class="brand_color_purple">dev@dreamspace.academy</span>.</p>
+
+	</div>
+</div>
+</div>
+</div>
+
+<?php include('9-0-footer.php'); ?>
